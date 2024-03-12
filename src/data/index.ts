f72c5bdbfc904cda706ef543bdab8acf7a1455e4
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-12 07:06:03 +0100

    feat: add footerMenus data items

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,15 +2,18 @@ import { FooterLinks } from '@/types';
 
 export const footerLinks: FooterLinks = [
   { id: 1, url: '/', label: 'Get Help' },
+  { id: 2, url: '/', label: 'Buy gift cards' },
+  { id: 3, url: '/', label: 'Add your restaurant' },
+  { id: 4, url: '/', label: 'Sign up to deliver' },
+  { id: 5, url: '/', label: 'Create a business account' },
+  { id: 6, url: '/', label: 'Promotions' },
+];
+
+export const footerMenus: FooterLinks = [
+  { id: 1, url: '/', label: 'View all cities' },
   { id: 2, url: '/', label: 'Restaurants near me' },
-  { id: 3, url: '/', label: 'Buy gift cards' },
-  { id: 4, url: '/', label: 'View all cities' },
-  { id: 5, url: '/', label: 'Add your restaurant' },
-  { id: 6, url: '/', label: 'View all countries' },
-  { id: 7, url: '/', label: 'Sign up to deliver' },
-  { id: 8, url: '/', label: 'Pickup near me' },
-  { id: 9, url: '/', label: 'Create a business account' },
-  { id: 10, url: '/', label: 'About Uber Eats' },
-  { id: 11, url: '/', label: 'Promotions' },
-  { id: 12, url: '/', label: 'English' },
+  { id: 3, url: '/', label: 'View all countries' },
+  { id: 4, url: '/', label: 'Pickup near me' },
+  { id: 5, url: '/', label: 'About Uber Eats' },
+  { id: 6, url: '/', label: 'English' },
 ];
