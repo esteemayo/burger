e7f8323b975faa32885dd9ad88201d02d96eda93
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-12 06:03:09 +0100

    fix: add html markup

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,50 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import Logo from '../logo/Logo';
+import { footerLinks } from '@/data';
+
 import './Footer.scss';
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      <div className='wrapper'>Footer</div>
+      <div className='wrapper'>
+        <div className='top'>
+          <div className='left'>
+            <div className=''>
+              <Logo />
+              <div className='download'>
+                <Image
+                  src='/svg/app-store.svg'
+                  width={135}
+                  height={40}
+                  alt='app store'
+                />
+                <Image
+                  src='/img/google-play.png'
+                  width={135}
+                  height={40}
+                  alt='google play store'
+                />
+              </div>
+            </div>
+          </div>
+          <div className='right'>
+            <div className='links'>
+              {footerLinks.map((link) => {
+                const { id, url, label } = link;
+                return (
+                  <Link key={id} href={url}>
+                    {label}
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className='bottom'></div>
+      </div>
     </footer>
   );
 };
