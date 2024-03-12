22c843fedf4521969448bb3ceca50bd4777242e0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-12 17:56:44 +0100

    feat: add Socials type annotation

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,15 @@
+import { SvgIconTypeMap } from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+
 export type FooterLinks = {
   id: number;
   url: string;
   label: string;
+}[];
+
+export type Socials = {
+  id: number;
+  icon: OverridableComponent<SvgIconTypeMap<{}, 'svg'>> & {
+    muiName: string;
+  };
 }[];
