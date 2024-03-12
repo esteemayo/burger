65e8cbb4ac7b43e56e8c8a8e1ab8a5d937611967
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-12 19:41:19 +0100

    feat: create Designer component

---
+++ src/components/designer/Designer.tsx
@@ -0,0 +1,17 @@
+import './Designer.scss';
+
+const Designer = () => {
+  return (
+    <div className='designer'>
+      <div className=''>
+        <div className='copyright'>
+          <span className='text'>
+            © Esteem Designs&nbsp;•Developed by Emmanuel Adebayo
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Designer;
