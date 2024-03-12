915a7d7a5c1351bc22e56d2757c5ec8a939074f2
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-12 05:51:33 +0100

    feat: add footerLinks data items

---
+++ src/data/index.ts
@@ -0,0 +1,16 @@
+import { FooterLinks } from '@/types';
+
+export const footerLinks: FooterLinks = [
+  { id: 1, label: 'Get Help' },
+  { id: 2, label: 'Restaurants near me' },
+  { id: 3, label: 'Buy gift cards' },
+  { id: 4, label: 'View all cities' },
+  { id: 5, label: 'Add your restaurant' },
+  { id: 6, label: 'View all countries' },
+  { id: 7, label: 'Sign up to deliver' },
+  { id: 8, label: 'Pickup near me' },
+  { id: 9, label: 'Create a business account' },
+  { id: 10, label: 'About Uber Eats' },
+  { id: 11, label: 'Promotions' },
+  { id: 12, label: 'English' },
+];
