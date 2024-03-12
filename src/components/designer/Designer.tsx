7b024c823246bcc44a8b3ade6b71a244b3357603
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-12 19:54:32 +0100

    fix: add styling

--- src/components/designer/Designer.tsx
+++ src/components/designer/Designer.tsx
@@ -3,10 +3,10 @@ import './Designer.scss';
 const Designer = () => {
   return (
     <div className='designer'>
-      <div className=''>
+      <div className='wrapper'>
         <div className='copyright'>
           <span className='text'>
-            © Esteem Designs&nbsp;•Developed by Emmanuel Adebayo
+            © Esteem Designs •&nbsp;Developed by Emmanuel Adebayo
           </span>
         </div>
       </div>
