f9f2509b8f622a5e5fb3c2c5256b6c8cf73c2ead
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-12 17:57:39 +0100

    feat: add socials data items

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,9 @@
-import { FooterLinks } from '@/types';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import XIcon from '@mui/icons-material/X';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+import { FooterLinks, Socials } from '@/types';
 
 export const footerLinks: FooterLinks = [
   { id: 1, url: '/', label: 'Get Help' },
@@ -16,4 +21,11 @@ export const footerMenus: FooterLinks = [
   { id: 4, url: '/', label: 'Pickup near me' },
   { id: 5, url: '/', label: 'About Uber Eats' },
   { id: 6, url: '/', label: 'English' },
+];
+
+export const socials: Socials = [
+  { id: 1, icon: FacebookIcon },
+  { id: 2, icon: XIcon },
+  { id: 3, icon: InstagramIcon },
+  { id: 4, icon: YouTubeIcon },
 ];
