21e8bc24e5ca8f48f56383349c3cb8e73bcc260a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-13 22:25:08 +0100

    feat: create Menus component

---
+++ src/components/menus/Menus.tsx
@@ -0,0 +1,11 @@
+import './Menus.scss';
+
+const Menus = () => {
+  return (
+    <section className='menus'>
+      <div className='container'>Menus</div>
+    </section>
+  );
+};
+
+export default Menus;
