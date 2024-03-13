7fd8be26cfca4635c97e9ea9ef65e1b480eb1b9c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-13 01:57:47 +0100

    feat: add html

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image';
+
 import './Header.scss';
 
 const Header = () => {
   return (
     <header className='header'>
-      <div className='container'>Header</div>
+      <div className='container'>
+        <h1>Search</h1>
+        <div className='search'>
+          <div className='searchInput'>
+            <Image
+              src='/img/search.png'
+              width={20}
+              height={20}
+              alt='search icon'
+              className='searchIcon'
+            />
+            <input type='text' className='search' placeholder='' />
+          </div>
+          <button className='searchBtn'>Find burger</button>
+        </div>
+      </div>
     </header>
   );
 };
