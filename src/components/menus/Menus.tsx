18ee408ac11079ad21c3967da64e0190ef12c69b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-13 22:35:57 +0100

    feat: add styling

--- src/components/menus/Menus.tsx
+++ src/components/menus/Menus.tsx
@@ -1,9 +1,20 @@
+import LocalDiningIcon from '@mui/icons-material/LocalDining';
+
 import './Menus.scss';
 
 const Menus = () => {
   return (
     <section className='menus'>
-      <div className='container'>Menus</div>
+      <div className='container'>
+        <div className='headings'>
+          <h2 className='heading'>Popular menu</h2>
+          <span className='left'>&nbsp;</span>
+          <div className='icon'>
+            <LocalDiningIcon />
+          </div>
+          <span className='right'>&nbsp;</span>
+        </div>
+      </div>
     </section>
   );
 };
