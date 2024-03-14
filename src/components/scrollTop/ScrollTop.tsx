81e9648a016a82a1f001ef74932369b6323ad2d2
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-15 00:05:48 +0100

    feat: add ScrollTop component

---
+++ src/components/scrollTop/ScrollTop.tsx
@@ -0,0 +1,15 @@
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+
+import './ScrollTop.scss';
+
+const ScrollTop = () => {
+  return (
+    <div className='scrollTop'>
+      <button type='button'>
+        <ExpandLessIcon />
+      </button>
+    </div>
+  );
+};
+
+export default ScrollTop;
