95f01d95ec2d2d263e9772917f658d9e923b743f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-14 01:33:59 +0100

    feat: add html markup

--- src/components/menuCard/MenuCard.tsx
+++ src/components/menuCard/MenuCard.tsx
@@ -1,7 +1,21 @@
+import Link from 'next/link';
 import './MenuCard.scss';
+import Image from 'next/image';
 
 const MenuCard = () => {
-  return <article className='menuCard'>MenuCard</article>;
+  return (
+    <article className='menuCard'>
+      <Link href='/'>
+        <div className='imgWrapper'>
+          <Image src='/img/hero.png' width={291} height={243} alt='' />
+        </div>
+        <div className='cardFooter'>
+          <h2 className='heading'>Double Grilled Chicken Burger</h2>
+          <span className='price'>$6,000.00</span>
+        </div>
+      </Link>
+    </article>
+  );
 };
 
 export default MenuCard;
