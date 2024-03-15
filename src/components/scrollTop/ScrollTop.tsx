431921c52f1de5d42a3e14336c0ed19853e0f6d0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-15 22:34:44 +0100

    feat: handle scroll to top functionality

--- src/components/scrollTop/ScrollTop.tsx
+++ src/components/scrollTop/ScrollTop.tsx
@@ -1,11 +1,38 @@
+'use client';
+
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import './ScrollTop.scss';
 
 const ScrollTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = useCallback(() => {
+    setIsVisible(window.scrollY > 500 ? true : false);
+  }, []);
+
+  const handleScroll = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, []);
+
+  const scrollClasses = useMemo(() => {
+    return isVisible ? 'scrollTop show' : 'scrollTop';
+  }, [isVisible]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, [toggleVisibility]);
+
   return (
-    <div className='scrollTop'>
-      <button type='button'>
+    <div className={scrollClasses}>
+      <button type='button' onClick={handleScroll}>
         <ExpandLessIcon />
       </button>
     </div>
