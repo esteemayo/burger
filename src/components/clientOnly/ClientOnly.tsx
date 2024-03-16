9fe2a90df964ada6456db69131bec5ff420319c7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-16 21:33:54 +0100

    feat: create ClientOnly component

---
+++ src/components/clientOnly/ClientOnly.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+const ClientOnly = ({ children }: { children: React.ReactNode }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return;
+  }
+
+  return <>{children}</>;
+};
+
+export default ClientOnly;
