030c0bfde4118406236b6f85969a363956cb5aec
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-16 20:08:52 +0100

    feat: dd html markup

--- src/components/offer/Offer.tsx
+++ src/components/offer/Offer.tsx
@@ -1,9 +1,23 @@
+import Image from 'next/image';
+
 import './Offer.scss';
 
 const Offer = () => {
   return (
     <section className='offer'>
-      <div className='container'>Offer</div>
+      <div className='container'>
+        <div className='left'>
+          <h1 className='header'>Double crunchy chicken burger</h1>
+          <p className='desc'>
+            Progressively simplify effective e-toilers and process-centric
+            methods of empowerment. Quickly pontificate parallel.
+          </p>
+          <button type='button'>Order now</button>
+        </div>
+        <div className='right'>
+          <Image src='/img/offerProduct.png' fill alt='offer product' />
+        </div>
+      </div>
     </section>
   );
 };
