ab1f45cd67f46fdd3e1bf892f714d62780ffe5cc
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-16 19:47:44 +0100

    feat: create register page component

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import './Register.scss';
+
+const Register = () => {
+  return (
+    <div className='register'>
+      <div className='container'>Register</div>
+    </div>
+  );
+};
+
+export default Register;
