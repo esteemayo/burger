6e4e03a6dc425cab32b2773429fa773a03fce990
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-16 19:44:53 +0100

    feat: create login page component

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import './Login.scss';
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <div className='container'>Login</div>
+    </div>
+  );
+};
+
+export default Login;
