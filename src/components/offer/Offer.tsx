aa0b0b67932f2a926d055ed0c02ac07fe656e196
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-16 19:52:51 +0100

    feat: create Offer component

---
+++ src/components/offer/Offer.tsx
@@ -0,0 +1,11 @@
+import './Offer.scss';
+
+const Offer = () => {
+  return (
+    <section className='offer'>
+      <div className='container'>Offer</div>
+    </section>
+  );
+};
+
+export default Offer;
