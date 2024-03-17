ee71126db4d31fa9092f719a4d902734114d9b18
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-17 01:02:44 +0100

    feat: create NewsLetter component

---
+++ src/components/newsletter/NewsLetter.tsx
@@ -0,0 +1,11 @@
+import './NewsLetter.scss';
+
+const NewsLetter = () => {
+  return (
+    <section className='newsLetter'>
+      <div className='container'>NewsLetter</div>
+    </section>
+  );
+};
+
+export default NewsLetter;
