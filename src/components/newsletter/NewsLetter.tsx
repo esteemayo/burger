4ac53b4c4c962ff46f575f20817716478df2fbf7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-17 01:12:35 +0100

    feat: add html markup

--- src/components/newsletter/NewsLetter.tsx
+++ src/components/newsletter/NewsLetter.tsx
@@ -1,9 +1,24 @@
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+
 import './NewsLetter.scss';
 
 const NewsLetter = () => {
   return (
     <section className='newsLetter'>
-      <div className='container'>NewsLetter</div>
+      <div className='container'>
+        <div className='textWrap'>
+          <div className='headingWrap'>
+            <EmailOutlinedIcon />
+            <h5>Subscribe to special offers</h5>
+          </div>
+        </div>
+        <div className='formWrap'>
+          <form>
+            <input type='text' placeholder='Your email address' />
+            <button type='submit'>Subscribe</button>
+          </form>
+        </div>
+      </div>
     </section>
   );
 };
