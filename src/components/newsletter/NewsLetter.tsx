868b19a6f5873544c0e8144f1275ac2e4a0fed96
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-17 02:08:07 +0100

    feat: add handleSubmit onsubmit event method

--- src/components/newsletter/NewsLetter.tsx
+++ src/components/newsletter/NewsLetter.tsx
@@ -1,8 +1,15 @@
+'use client';
+
+import { useCallback } from 'react';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 
 import './NewsLetter.scss';
 
 const NewsLetter = () => {
+  const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  }, []);
+
   return (
     <section className='newsLetter'>
       <div className='container'>
@@ -13,7 +20,7 @@ const NewsLetter = () => {
           </div>
         </div>
         <div className='formWrap'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <input type='email' placeholder='Your email address' />
             <button type='submit'>Subscribe</button>
           </form>
