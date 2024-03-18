c8ad9424712620f1c08b597617f797b21326b6fb
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-18 15:57:54 +0100

    feat: add styling

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,17 @@
+import RegisterForm from './RegisterForm';
+import RegisterImage from './RegisterImage';
+
 import './Register.scss';
 
 const Register = () => {
   return (
     <div className='register'>
-      <div className='container'>Register</div>
+      <div className='container'>
+        <div className='wrapper'>
+          <RegisterImage />
+          <RegisterForm />
+        </div>
+      </div>
     </div>
   );
 };
