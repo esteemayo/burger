952bbc9881d886c1a0e55f4f1ee4587dd47c6993
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-19 04:48:01 +0100

    feat: add ProductLists component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,15 @@
+import ProductLists from '@/components/productLists/ProductLists';
+
 import './Products.scss';
 
 const Products = () => {
-  return <div className='products'>Products</div>;
+  return (
+    <div className='products'>
+      <div className='container'>
+        <ProductLists />
+      </div>
+    </div>
+  );
 };
 
 export default Products;
