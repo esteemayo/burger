73c12a0b6a5ff33919fa999be439f7b8c5893c36
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-19 17:48:38 +0100

    fix: modify

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useMemo } from 'react';
+
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -6,10 +10,14 @@ import { formatCurrency } from '@/utils/formatCurrency';
 
 import './ProductCard.scss';
 
-const ProductCard = ({ name, image, price }: ProductCardProps) => {
+const ProductCard = ({ id, name, image, price }: ProductCardProps) => {
+  const url = useMemo(() => {
+    return `/product/${encodeURIComponent(id)}`;
+  }, [id]);
+
   return (
     <article className='productCard'>
-      <Link href='/'>
+      <Link href={url}>
         <div className='imgWrapper'>
           <span className='overlay'>
             <button type='button' className='cartButton'>
