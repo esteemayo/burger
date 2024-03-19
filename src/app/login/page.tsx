69160af258ba0dd5ed11248c2ab2af114249b0b7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-19 04:30:23 +0100

    feat: handle SEO

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,15 @@
+import type { Metadata } from 'next';
+
 import LoginForm from './LoginForm';
 import LoginImage from './LoginImage';
 
 import './Login.scss';
+
+export const metadata: Metadata = {
+  title: 'Burger - Login page',
+  description: 'Log into your account',
+  keywords: 'login, signin, burger, account, auth',
+};
 
 const Login = () => {
   return (
