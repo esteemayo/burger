239a92e1e8df9147ad01068d3956da951907b604
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-19 04:53:54 +0100

    feat: iterate over product data array

--- src/components/productLists/ProductLists.tsx
+++ src/components/productLists/ProductLists.tsx
@@ -1,7 +1,16 @@
+import { products } from '@/data';
+import ProductCard from '../productCard/ProductCard';
+
 import './ProductLists.scss';
 
 const ProductLists = () => {
-  return <section className='productLists'>ProductLists</section>;
+  return (
+    <section className='productLists'>
+      {products.map((product) => {
+        return <ProductCard key={product.id} {...product} />;
+      })}
+    </section>
+  );
 };
 
 export default ProductLists;
