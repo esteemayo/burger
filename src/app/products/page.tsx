fe5b62df966ad33da45e61fa76f9ec3ff6249c6a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-19 16:41:08 +0100

    feat: add html markup

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import ProductLists from '@/components/productLists/ProductLists';
 
 import './Products.scss';
@@ -6,6 +8,12 @@ const Products = () => {
   return (
     <div className='products'>
       <div className='container'>
+        <h3 className='heading'>
+          Most popular near you
+          <small>
+            <Link href='/products'>12 main features</Link>
+          </small>
+        </h3>
         <ProductLists />
       </div>
     </div>
