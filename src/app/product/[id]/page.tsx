d02a2d5e771a916d3b37de6f017a76f2f7466507
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-20 02:20:50 +0100

    feat: create single product page component

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,11 @@
+import './Product.scss';
+
+const Product = () => {
+  return (
+    <div className='product'>
+      <div className='container'>Product</div>
+    </div>
+  );
+};
+
+export default Product;
