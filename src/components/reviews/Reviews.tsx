6e596677bf046b55170c229c0e62c1111f8459e2
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-20 17:28:13 +0100

    feat: add htmlmarkup

--- src/components/reviews/Reviews.tsx
+++ src/components/reviews/Reviews.tsx
@@ -3,7 +3,47 @@ import './Reviews.scss';
 const Reviews = () => {
   return (
     <div className='reviews'>
-      <div className='container'>Reviews</div>
+      <div className='container'>
+        <ul className='listWrap'>
+          <li>Reviews (0)</li>
+        </ul>
+        <div className='reviewWrap'>
+          <div className='reviewContainer'>
+            <div className='allReviews'>
+              <p>There are no reviews yet.</p>
+            </div>
+            <div className='reviewFormWrap'>
+              <div className='reviewForm'>
+                <div className='respond'>
+                  <span className='replyTitle'>
+                    Be the first to review “Chicken Caesar Salad”
+                  </span>
+                  <form>
+                    <textarea name='review' id='review' cols={30} rows={10} />
+                    <div>
+                      <input type='text' name='name' placeholder='Name' />
+                      <input type='email' name='email' placeholder='Email' />
+                    </div>
+                    <div className='consent'>
+                      <p>
+                        Save my name, email, and website in this browser for the
+                        next time I comment.
+                      </p>
+                      <div>
+                        <input type='checkbox' name='consent' id='consent' />
+                        <label htmlFor='consent'>
+                          Sign me up for the newsletter!
+                        </label>
+                      </div>
+                      <button type='submit'>Submit review</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
