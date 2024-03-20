bb602cb61fe11f78f5a5960c10c05df346fa7686
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-20 03:14:03 +0100

    feat: add html markup

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -3,6 +3,12 @@ import './Product.scss';
 const Product = () => {
   return (
     <div className='product'>
+      <header>
+        <h1 className='productHeading'>Double grilled chicken burger</h1>
+        <div className='productSubHeading'>
+          <span>Welcome to burger</span>
+        </div>
+      </header>
       <div className='container'>Product</div>
     </div>
   );
