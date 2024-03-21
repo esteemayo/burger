afeaffa292afc9edc16ee9f619509a1545cb7c70
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-21 18:28:58 +0100

    feat: add html markup

--- src/components/review/Review.tsx
+++ src/components/review/Review.tsx
@@ -1,7 +1,40 @@
+import Image from 'next/image';
+import { FaStar } from 'react-icons/fa';
+
 import './Review.scss';
 
 const Review = () => {
-  return <article className='review'>Review</article>;
+  return (
+    <article className='review'>
+      <div className='reviewCard'>
+        <div className='reviewWrapper'>
+          <div className='reviewImg'>
+            <Image
+              src='/svg/male-avatar.svg'
+              width={87}
+              height={87}
+              alt='avatar'
+            />
+          </div>
+          <div className='reviewer'>
+            <div className='reviewRating'>
+              <div className='star'>
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
+            </div>
+            <div>
+              <div className='reviewerName'>Emmanuel Adebayo</div>
+              <p className='reviewerText'>Looks so delicious!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default Review;
