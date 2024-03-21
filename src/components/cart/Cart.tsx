32b5f033a1977151a47d65c3a34a2357c357eae6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-21 17:21:11 +0100

    feat: add html markup

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,10 +1,13 @@
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import Link from 'next/link';
 import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import './Cart.scss';
 
 const Cart = () => {
+  const cart = [{ id: 1, name: 'Grilled chicken burger', price: 11999.0 }];
+
   return (
     <aside className='productCart'>
       <div className='detailBox'>
@@ -12,35 +15,46 @@ const Cart = () => {
           <div className='cardHeading'>Your cart</div>
           <div className='cardBody'>
             <div className='catProduct'>
-              <div className='cardBox'>
-                <div className='cardProductWrap'>
-                  <div className='cardProduct'>
-                    <div className='cardName'>
-                      <p>
-                        <span>1</span>
-                        Grilled chicken burger
-                      </p>
-                    </div>
-                    <div className='cardButtons'>
-                      <button type='button'>
-                        <ExpandMoreIcon />
-                      </button>
-                      <button type='button'>
-                        <ExpandLessIcon />
-                      </button>
-                    </div>
-                    <button type='button' className='deleteCardBtn'>
-                      <RemoveShoppingCartIcon />
-                    </button>
-                    <div className='cardPrice'>$11999.0</div>
+              {cart.length > 0 ? (
+                <div className='emptyCart'>
+                  <div className='emptyHeading'>Empty cart</div>
+                  <div className='emptyLink'>
+                    <Link href='/'>Continue shopping</Link>
                   </div>
                 </div>
-              </div>
+              ) : (
+                <div className='cardBox'>
+                  <div className='cardProductWrap'>
+                    <div className='cardProduct'>
+                      <div className='cardName'>
+                        <p>
+                          <span>1</span>
+                          Grilled chicken burger
+                        </p>
+                      </div>
+                      <div className='cardButtons'>
+                        <button type='button'>
+                          <ExpandMoreIcon />
+                        </button>
+                        <button type='button'>
+                          <ExpandLessIcon />
+                        </button>
+                      </div>
+                      <button type='button' className='deleteCardBtn'>
+                        <RemoveShoppingCartIcon />
+                      </button>
+                      <div className='cardPrice'>$11999.0</div>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
-          <div className='cardFooter'>
-            <button type='button'>Proceed to Checkout</button>
-          </div>
+          {cart.length < 1 && (
+            <div className='cardFooter'>
+              <button type='button'>Proceed to Checkout</button>
+            </div>
+          )}
         </div>
       </div>
     </aside>
