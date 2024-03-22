558e82eb5b09d8bdf12790b4e5687b298af2c928
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-22 13:03:10 +0100

    feat: create RelatedProducts component

---
+++ src/components/relatedProducts/RelatedProducts.tsx
@@ -0,0 +1,11 @@
+import './RelatedProducts.scss';
+
+const RelatedProducts = () => {
+  return (
+    <section className='relatedProducts'>
+      <div className='container'>RelatedProducts</div>
+    </section>
+  );
+};
+
+export default RelatedProducts;
