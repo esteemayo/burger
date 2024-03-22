eabca54c8b375fd282265b47f66a73a9128fe132
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-22 14:07:44 +0100

    feat: add html markup

--- src/components/relatedProduct/RelatedProduct.tsx
+++ src/components/relatedProduct/RelatedProduct.tsx
@@ -1,7 +1,29 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 import './RelatedProduct.scss';
 
 const RelatedProduct = () => {
-  return <article className='relatedProduct'>RelatedProduct</article>;
+  return (
+    <article className='relatedProduct'>
+      <div className=''>
+        <Link href={`/product/1`}>
+          <Image
+            src='/img/hero.png'
+            width={190}
+            height={170}
+            alt='product image'
+          />
+        </Link>
+        <div className='relatedContent'>
+          <Link href={`/product/1`}>
+            <h2 className='relatedHeading'>Grilled Chicken Burger</h2>
+            <span className='relatedPrice'>$4,000.00</span>
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default RelatedProduct;
