7ce0aa63b97e7fc2824233d169accbd08e80dea6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-22 13:18:32 +0100

    feat: add styling

--- src/components/relatedProducts/RelatedProducts.tsx
+++ src/components/relatedProducts/RelatedProducts.tsx
@@ -1,9 +1,16 @@
+import RelatedProduct from '../relatedProduct/RelatedProduct';
+
 import './RelatedProducts.scss';
 
 const RelatedProducts = () => {
   return (
     <section className='relatedProducts'>
-      <div className='container'>RelatedProducts</div>
+      <div className='container'>
+        <h2 className='relatedHeader'>Related products</h2>
+        <div className='relatedWrap'>
+          <RelatedProduct />
+        </div>
+      </div>
     </section>
   );
 };
