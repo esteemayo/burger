f7db538cec09971b5a872d08ee443df845fa984f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-23 00:59:47 +0100

    feat: create Hero component

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,16 @@
+import './Hero.scss';
+
+const Hero = () => {
+  return (
+    <header className='hero'>
+      <div className='headingWrap'>
+        <h1 className='heroHeading'>Double grilled chicken burger</h1>
+        <div className='heroSubHeading'>
+          <span className='heroText'>Welcome to burger</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
