ed01fc4250bd046878f4d367cf76049d576be64e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-23 00:52:52 +0100

    feat: render component' dynamically

--- src/components/relatedProduct/RelatedProduct.tsx
+++ src/components/relatedProduct/RelatedProduct.tsx
@@ -1,9 +1,19 @@
-import Link from 'next/link';
+'use client';
+
 import Image from 'next/image';
+import Link from 'next/link';
+import { useMemo } from 'react';
+
+import { RelatedProductProps } from '@/types';
+import { formatCurrency } from '@/utils/formatCurrency';
 
 import './RelatedProduct.scss';
 
-const RelatedProduct = () => {
+const RelatedProduct = ({ id, name, image, price }: RelatedProductProps) => {
+  const url = useMemo(() => {
+    return `/product/${encodeURIComponent(id)}`;
+  }, [id]);
+
   return (
     <article className='relatedProduct'>
       <div className='relatedBox'>
@@ -13,17 +23,12 @@ const RelatedProduct = () => {
               Add to cart
             </button>
           </span>
-          <Image
-            src='/img/hero.png'
-            width={220}
-            height={170}
-            alt='product image'
-          />
+          <Image src={image} width={220} height={170} alt={name} />
         </div>
         <div className='relatedContent'>
-          <Link href={`/product/1`}>
-            <h2 className='relatedHeading'>Grilled Chicken Burger</h2>
-            <span className='relatedPrice'>$4,000.00</span>
+          <Link href={url}>
+            <h2 className='relatedHeading'>{name}</h2>
+            <span className='relatedPrice'>{formatCurrency(price)}</span>
           </Link>
         </div>
       </div>
