fd18cb353b530ed903e77cb0506f9e0f1e0f5962
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-23 01:05:08 +0100

    fix: modify

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import { useMemo } from 'react';
-
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useMemo } from 'react';
 
 import { ProductCardProps } from '@/types';
 import { formatCurrency } from '@/utils/formatCurrency';
