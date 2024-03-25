d66a055e0d6ba4c0e69074b8c76f883122b5749c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-25 18:01:05 +0100

    fix: refactor

--- src/components/accountInfo/AccountInfo.tsx
+++ src/components/accountInfo/AccountInfo.tsx
@@ -1,46 +1,14 @@
-import Image from 'next/image';
+import { accountCards } from '@/data';
+import AccountCard from '../accountCard/AccountCard';
 
 import './AccountInfo.scss';
 
 const AccountInfo = () => {
   return (
     <div className='accountInfo'>
-      <article className='accountCard'>
-        <div className='left'>
-          <h2>461</h2>
-          <span>Days Since Joining</span>
-        </div>
-        <div className='right'>
-          <Image src='/svg/male-avatar.svg' width={25} height={25} alt='' />
-        </div>
-      </article>
-      <article className='accountCard'>
-        <div className='left'>
-          <h2>$0.00</h2>
-          <span>Amount in Wallet</span>
-        </div>
-        <div className='right'>
-          <Image src='/svg/male-avatar.svg' width={25} height={25} alt='' />
-        </div>
-      </article>
-      <article className='accountCard'>
-        <div className='left'>
-          <h2>12</h2>
-          <span>Orders</span>
-        </div>
-        <div className='right'>
-          <Image src='/svg/male-avatar.svg' width={25} height={25} alt='' />
-        </div>
-      </article>
-      <article className='accountCard'>
-        <div className='left'>
-          <h2>157</h2>
-          <span>Loyalty Points</span>
-        </div>
-        <div className='right'>
-          <Image src='/svg/male-avatar.svg' width={25} height={25} alt='' />
-        </div>
-      </article>
+      {accountCards.map((item) => {
+        return <AccountCard key={item.id} {...item} />;
+      })}
     </div>
   );
 };
