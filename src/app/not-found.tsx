28e126c65545aaee4040d2a21f65f5ddd6daafd3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-25 14:47:29 +0100

    feat: add Heading component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+import Heading from '@/components/heading/Heading';
+
 import './NotFound.scss';
 
 const NotFound = () => {
@@ -9,6 +11,11 @@ const NotFound = () => {
       <div className='container'>
         <Link href='/'>
           <Image src='/svg/blank.svg' width={300} height={300} alt='404' />
+          <Heading
+            title='Page not found!'
+            subtitle='Try again later.'
+            center={true}
+          />
         </Link>
       </div>
     </div>
