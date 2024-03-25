251dd81264aea999a95da2f1cdc4e49da23336a8
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-25 02:01:46 +0100

    feat: create Heading component

---
+++ src/components/heading/Heading.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useMemo } from 'react';
+
+import { HeadingProps } from '@/types';
+
+import './Heading.scss';
+
+const Heading = ({ title, subtitle, center }: HeadingProps) => {
+  const headingClasses = useMemo(() => {
+    return center ? 'heading center' : 'heading';
+  }, [center]);
+
+  return (
+    <div className={headingClasses}>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+    </div>
+  );
+};
+
+export default Heading;
