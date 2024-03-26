553cde642b23a6400347f42ac0b24b7dbdc99e46
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-26 14:26:07 +0100

    feat: add html markup

--- src/components/deactivateAccount/DeactivateAccount.tsx
+++ src/components/deactivateAccount/DeactivateAccount.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
+
 import './DeactivateAccount.scss';
 
 const DeactivateAccount = () => {
-  return <div className='deactivateAccount'>DeactivateAccount</div>;
+  return (
+    <div className='deactivateAccount'>
+      <button type='button'>
+        <Image src='/' width={15} height={15} alt='icon' />
+        Delete my Account
+      </button>
+    </div>
+  );
 };
 
 export default DeactivateAccount;
