5aa7c140c8682d5f047d65a8c167d758b332d8ac
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-26 13:40:37 +0100

    feat: add DeactivateAccount component

--- src/components/accountDetails/AccountDetails.tsx
+++ src/components/accountDetails/AccountDetails.tsx
@@ -1,5 +1,6 @@
-import AccountData from '../accountData/AccountData';
 import AccountPassword from '../accountPassword/AccountPassword';
+import AccountData from '../accountData/AccountData';
+import DeactivateAccount from '../deactivateAccount/DeactivateAccount';
 
 import './AccountDetails.scss';
 
@@ -8,6 +9,7 @@ const AccountDetails = () => {
     <div className='accountDetails'>
       <AccountData />
       <AccountPassword />
+      <DeactivateAccount />
     </div>
   );
 };
