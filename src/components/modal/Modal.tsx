fab9398eacc21ac19a8ceb65f846eea0e07a5d76
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-27 16:10:14 +0100

    feat: create Modal component

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,13 @@
+import './Modal.scss';
+
+const Modal = () => {
+  return (
+    <div className='modal'>
+      <div className='box'>
+        <div className='wrapper'>Modal</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
