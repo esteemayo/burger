af98d9f5471d30814177577ed5812fcae4238cd3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-27 16:35:43 +0100

    feat: add html markup

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,10 +1,32 @@
+import Image from 'next/image';
+
 import './Modal.scss';
 
 const Modal = () => {
   return (
     <aside className='modal'>
-      <div className='box'>
-        <div className='wrapper'>Modal</div>
+      <div className='box active'>
+        <div className='wrapper'>
+          <h1 className='modalHeading'>Header</h1>
+          <div className='modalBody'>Body</div>
+          <div className='modalFooter'>
+            <div className='modalBtnWrap'>
+              <button type='button'>Prev</button>
+              <button type='button'>Next</button>
+            </div>
+            Footer
+          </div>
+          <div className='modalCloseBtnWrap'>
+            <button type='button' className='closeBtn'>
+              <Image
+                src='/svg/x-mark.svg'
+                width={20}
+                height={20}
+                alt='close icon'
+              />
+            </button>
+          </div>
+        </div>
       </div>
     </aside>
   );
