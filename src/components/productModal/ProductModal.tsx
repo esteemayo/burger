9d65d79c4d4f01f90e0588c1e97f8729a0ddd6f1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-28 00:55:51 +0100

    feat: create ProductModal component

---
+++ src/components/productModal/ProductModal.tsx
@@ -0,0 +1,13 @@
+import Modal from '../modal/Modal';
+import { useProductModal } from '@/hooks/useProductModal';
+
+import './ProductModal.scss';
+
+const ProductModal = () => {
+  const isOpen = useProductModal((state) => state.isOpen);
+  const onClose = useProductModal((state) => state.onClose);
+
+  return <Modal isOpen={isOpen} actionLabel='Create' />;
+};
+
+export default ProductModal;
