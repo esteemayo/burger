21e6a034c228beda3172c65edc9eeea50eb7e0fa
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-27 04:40:13 +0100

    feat: add toast provider

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,12 +3,14 @@ import Designer from '../designer/Designer';
 import Navbar from '../navbar/Navbar';
 import ClientOnly from '../clientOnly/ClientOnly';
 import ScrollTop from '../scrollTop/ScrollTop';
+import ToasterProvider from '@/providers/ToasterProvider';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
       <ClientOnly>
         <Navbar />
+        <ToasterProvider />
         {children}
         <Footer />
         <Designer />
