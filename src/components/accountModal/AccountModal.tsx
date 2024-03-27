6f4a122bf1d7919d25246d0f8b51e35ee56b47ad
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-27 18:24:39 +0100

    feat: add html markup

--- src/components/accountModal/AccountModal.tsx
+++ src/components/accountModal/AccountModal.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useCallback } from 'react';
+
 import Modal from '../modal/Modal';
 import { useAccountModal } from '@/hooks/useAccountModal';
 
@@ -9,7 +11,27 @@ const AccountModal = () => {
   const isOpen = useAccountModal((state) => state.isOpen);
   const onClose = useAccountModal((state) => state.onClose);
 
-  return <Modal isOpen={isOpen} onClose={onClose} />;
+  const onSubmit = useCallback(() => {}, []);
+
+  const bodyContent: React.ReactElement = (
+    <div className='accountModal'>
+      <p>
+        Deleting your account will remove all of your information from our
+        database. This cannot be undone.
+      </p>
+    </div>
+  );
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      title='Delete Account'
+      actionLabel='Confirm deletion'
+      body={bodyContent}
+      onClose={onClose}
+      onSubmit={onSubmit}
+    />
+  );
 };
 
 export default AccountModal;
