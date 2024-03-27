e0cc42716d29e41b70c709dbb75757b6fa00bd7e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-27 18:03:57 +0100

    fix: modify

--- src/components/deactivateAccount/DeactivateAccount.tsx
+++ src/components/deactivateAccount/DeactivateAccount.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image';
 
+import { useAccountModal } from '@/hooks/useAccountModal';
+
 import './DeactivateAccount.scss';
 
 const DeactivateAccount = () => {
+  const onOpen = useAccountModal((state) => state.onOpen);
+
   return (
     <div className='deactivateAccount'>
-      <button type='button'>
+      <button type='button' onClick={onOpen}>
         <Image src='/svg/user-minus.svg' width={15} height={15} alt='icon' />
         Delete my Account
       </button>
