925440ccaed753c293817a8c230a88bae56a9aa4
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-27 18:33:42 +0100

    feat: add styling

--- src/components/accountModal/AccountModal.tsx
+++ src/components/accountModal/AccountModal.tsx
@@ -15,7 +15,7 @@ const AccountModal = () => {
 
   const bodyContent: React.ReactElement = (
     <div className='accountModal'>
-      <p>
+      <p classname='accountModalText' style={{ fontSize: '1.3rem', color: '#00000059' }}>
         Deleting your account will remove all of your information from our
         database. This cannot be undone.
       </p>
