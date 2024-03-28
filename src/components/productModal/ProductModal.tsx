abb3ca46917370937e12f9be41ca88a0152380e1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-28 01:29:22 +0100

    feat: add onSubmit handler function

--- src/components/productModal/ProductModal.tsx
+++ src/components/productModal/ProductModal.tsx
@@ -1,15 +1,45 @@
 'use client';
 
+import { useCallback, useState } from 'react';
+
 import Modal from '../modal/Modal';
 import { useProductModal } from '@/hooks/useProductModal';
 
 import './ProductModal.scss';
 
+const enum STEPS {
+  INFO = 0,
+  IMAGE = 1,
+}
+
 const ProductModal = () => {
   const isOpen = useProductModal((state) => state.isOpen);
   const onClose = useProductModal((state) => state.onClose);
 
-  return <Modal isOpen={isOpen} actionLabel='Create' />;
+  const [step, setStep] = useState(STEPS.INFO);
+
+  const onPrev = useCallback(() => {
+    setStep((value) => {
+      return value - 1;
+    });
+  }, []);
+
+  const onNext = useCallback(() => {
+    setStep((value) => {
+      return value + 1;
+    });
+  }, []);
+
+  const onSubmit = useCallback(() => {}, []);
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      actionLabel='Create'
+      onClose={onClose}
+      onSubmit={onSubmit}
+    />
+  );
 };
 
 export default ProductModal;
