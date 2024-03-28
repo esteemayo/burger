e16fb5fa31272e74c650bd56fabfddec25235e97
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-28 01:46:26 +0100

    fix: modify

--- src/components/newProduct/NewProduct.tsx
+++ src/components/newProduct/NewProduct.tsx
@@ -1,11 +1,17 @@
+'use client';
+
 import Image from 'next/image';
+
+import { useProductModal } from '@/hooks/useProductModal';
 
 import './NewProduct.scss';
 
 const NewProduct = () => {
+  const onOpen = useProductModal((state) => state.onOpen);
+
   return (
     <div className='newProduct'>
-      <button type='button'>
+      <button type='button' onClick={onOpen}>
         <Image src='/svg/plus.svg' width={15} height={15} alt='plus icon' />
         Add new product
       </button>
