b544b8ba0d699d41fa5544aafed28c181ff17627
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-28 01:38:50 +0100

    feat: create NewProduct component

--- src/components/accountDetails/AccountDetails.tsx
+++ src/components/accountDetails/AccountDetails.tsx
@@ -1,15 +1,18 @@
 import AccountPassword from '../accountPassword/AccountPassword';
-import AccountData from '../accountData/AccountData';
+import NewProduct from '../newProduct/NewProduct';
 import DeactivateAccount from '../deactivateAccount/DeactivateAccount';
+import AccountData from '../accountData/AccountData';
 
 import './AccountDetails.scss';
 
 const AccountDetails = () => {
+  const isAdmin = true;
+
   return (
     <div className='accountDetails'>
       <AccountData />
       <AccountPassword />
-      <DeactivateAccount />
+      {isAdmin ? <NewProduct /> : <DeactivateAccount />}
     </div>
   );
 };
