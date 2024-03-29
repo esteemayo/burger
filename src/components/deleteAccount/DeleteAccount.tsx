7ac256dfbd67f9dd52876cabc77166a6ab2a30da
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-29 01:23:39 +0100

    feat: create DeleteAccount component

---
+++ src/components/deleteAccount/DeleteAccount.tsx
@@ -0,0 +1,14 @@
+import './DeleteAccount.scss';
+
+const DeleteAccount = () => {
+  return (
+    <div className='deleteAccount'>
+      <p className='deleteAccountText'>
+        Deleting your account will remove all of your information from our
+        database. This cannot be undone.
+      </p>
+    </div>
+  );
+};
+
+export default DeleteAccount;
