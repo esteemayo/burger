183cdfd58e7a76c19c0376fca9272003f370f977
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-31 14:22:48 +0100

    feat: add dropZone

--- src/components/dropZone/DropZone.tsx
+++ src/components/dropZone/DropZone.tsx
@@ -1,7 +1,35 @@
+'use client';
+
+import { useCallback, useEffect } from 'react';
+import { useDropzone } from 'react-dropzone';
+
 import './DropZone.scss';
 
 const DropZone = () => {
-  return <div className='dropZone'>DropZone</div>;
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+    console.log(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
+    useDropzone({ onDrop });
+
+  useEffect(() => {
+    console.log(acceptedFiles);
+  }, [acceptedFiles]);
+
+  return (
+    <div className='dropZone'>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default DropZone;
