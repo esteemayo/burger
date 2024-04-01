ae72bd757da815c3dacf51fcb603c8d80304ad63
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-01 14:58:58 +0100

    feat: add styling

--- src/components/accountCard/AccountCard.tsx
+++ src/components/accountCard/AccountCard.tsx
@@ -1,12 +1,22 @@
+'use client';
+
 import Image from 'next/image';
+import { useMemo } from 'react';
 
 import { AccountCardProps } from '@/types';
+import { useAccountMenu } from '@/hooks/useAccountMenu';
 
 import './AccountCard.scss';
 
 const AccountCard = ({ icon, count, price, label }: AccountCardProps) => {
+  const isOpen = useAccountMenu((state) => state.isOpen);
+
+  const cardClasses = useMemo(() => {
+    return isOpen ? 'accountCard toggle' : 'accountCard';
+  }, [isOpen]);
+
   return (
-    <article className='accountCard'>
+    <article className={cardClasses}>
       <div className='statsWrap'>
         <h2>{count ?? price}</h2>
         <span>{label}</span>
