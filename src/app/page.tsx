4179f54ba638882718abd5f18998484d4f71a6bb
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-01 15:36:33 +0100

    feat: handle SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import type { Metadata } from 'next';
+
 import Header from '@/components/header/Header';
 import Menus from '@/components/menus/Menus';
 import Features from '@/components/features/Features';
 import Offer from '@/components/offer/Offer';
 import NewsLetter from './../components/newsletter/NewsLetter';
+
+export const metadata: Metadata = {
+  title: 'Burger - Home page',
+};
 
 const Home = () => {
   return (
