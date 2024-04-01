e2d3300c2ac64c8a2c8a438492a1b03007496229
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-01 14:23:41 +0100

    feat: add useAccountMenu store

---
+++ src/hooks/useAccountMenu.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand';
+import { produce } from 'immer';
+import { devtools } from 'zustand/middleware';
+
+import { AccountMenuStore, AccountMenuType } from '@/types';
+
+const INITIAL_STATE = {
+  isOpen: false,
+};
+
+export const useAccountMenu = create<AccountMenuStore & AccountMenuType>()(
+  devtools((set) => ({
+    isOpen: INITIAL_STATE.isOpen,
+    onOpen: () =>
+      set(
+        produce((state) => {
+          state.isOpen = true;
+        }),
+        false,
+        'openMenu'
+      ),
+    onClose: () =>
+      set(
+        produce((state) => {
+          state.isOpen = false;
+        }),
+        false,
+        'closeMenu'
+      ),
+  }))
+);
