fbd3408497ea5034d3f796b4515609dfd184e820
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-02 19:39:01 +0100

    feat: handle add product to cart functionality

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -2,32 +2,48 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 
+import { useCartStore } from '@/hooks/useCartStore';
 import { ProductCardProps } from '@/types';
 import { formatCurrency } from '@/utils/formatCurrency';
 
 import './ProductCard.scss';
 
-const ProductCard = ({ id, name, image, price }: ProductCardProps) => {
+const ProductCard = ({ product }: ProductCardProps) => {
+  const addToCart = useCartStore((state) => state.addToCart);
+
+  const handleClick = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+      addToCart({ ...product, quantity: 1 });
+    },
+    [addToCart, product]
+  );
+
   const url = useMemo(() => {
-    return `/product/${encodeURIComponent(id)}`;
-  }, [id]);
+    return `/product/${encodeURIComponent(product.id)}`;
+  }, [product.id]);
 
   return (
     <article className='productCard'>
       <div className='imgWrapper'>
         <span className='overlay'>
-          <button type='button' className='cartButton'>
+          <button type='button' className='cartButton' onClick={handleClick}>
             Add to cart
           </button>
         </span>
-        <Image src={image} width={300} height={270} alt={name} />
+        <Image
+          src={product.image}
+          width={300}
+          height={270}
+          alt={product.name}
+        />
       </div>
       <div className='cardFooter'>
         <Link href={url}>
-          <h2 className='heading'>{name}</h2>
-          <span className='price'>{formatCurrency(price)}</span>
+          <h2 className='heading'>{product.name}</h2>
+          <span className='price'>{formatCurrency(product.price)}</span>
         </Link>
       </div>
     </article>
