0c446a1c09aa46047728f5614829cdacdf1e9cbd
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-03 01:15:25 +0100

    feat: create Checkout component

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,11 @@
+import './Checkout.scss';
+
+const Checkout = () => {
+  return (
+    <div className='checkout'>
+      <div className='container'>Checkout</div>
+    </div>
+  );
+};
+
+export default Checkout;
