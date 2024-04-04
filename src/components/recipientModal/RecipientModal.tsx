aec2ad55bd4aa056eef37e73daa4c7bd8a5dc423
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-04 21:30:47 +0100

    feat: add Input fields

--- src/components/recipientModal/RecipientModal.tsx
+++ src/components/recipientModal/RecipientModal.tsx
@@ -2,7 +2,9 @@
 
 import { useCallback } from 'react';
 
+import Input from '../input/Input';
 import Modal from '../modal/Modal';
+
 import { useRecipient } from '@/hooks/useRecipientModal';
 
 import './RecipientModal.scss';
@@ -15,11 +17,39 @@ const RecipientModal = () => {
     console.log('submit');
   }, []);
 
+  let bodyContent: JSX.Element;
+
+  bodyContent = (
+    <>
+      <Input
+        name='name'
+        label='Name'
+        placeholder='Name'
+        onChange={() => console.log('first')}
+      />
+      <Input
+        name='email'
+        type='email'
+        label='Email'
+        placeholder='Email address'
+        onChange={() => console.log('first')}
+      />
+      <Input
+        name='phone'
+        type='tel'
+        label='Phone Number'
+        placeholder='Phone number'
+        onChange={() => console.log('first')}
+      />
+    </>
+  );
+
   return (
     <Modal
+      isOpen={isOpen}
       title='Update Recipient'
       actionLabel='Submit'
-      isOpen={isOpen}
+      body={bodyContent}
       onClose={onClose}
       onSubmit={onSubmit}
     />
