442b023196beeb4eb66d5757d676c47b1b696e67
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-04 16:28:47 +0100

    feat: add html markup

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import './Checkout.scss';
 
 const Checkout = () => {
@@ -33,7 +34,56 @@ const Checkout = () => {
               <button type='button'>Place your order</button>
             </div>
           </div>
-          <div className='checkoutTotal'>total</div>
+          <div className='checkoutTotal'>
+            <div className='checkoutWrap'>
+              <div className='checkoutDetail'>
+                <div className='checkoutCard'>
+                  <div className='checkoutHeader'>
+                    Your order from
+                    <p className='checkoutCustomer'>Emmanuel adebayo</p>
+                  </div>
+                  <div className='checkoutBody'>
+                    <div className='checkoutProductBox'>
+                      <div className='checkoutProduct'>
+                        <div className='checkoutProductName'>
+                          <Link href='/'>Double beef burger</Link>
+                          <div className='checkoutQty'>
+                            <button type='button'>-</button>
+                            <span className='qty'>1</span>
+                            <button type='button'>+</button>
+                          </div>
+                        </div>
+                        <div className='checkoutDeleteWrap'>
+                          <button type='button'>X</button>
+                        </div>
+                        <div className='checkoutPrice'>
+                          <span>$12999.00</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='checkoutTotal'>
+                      <div className='checkoutTotalPrice'>
+                        <span>Items total</span>
+                        <span>$12999.00</span>
+                      </div>
+                      <div className='checkoutTotalPrice'>
+                        <span>Discount</span>
+                        <span>$0.00</span>
+                      </div>
+                      <div className='checkoutTotalPrice'>
+                        <span>Delivery charge</span>
+                        <span>$0.00</span>
+                      </div>
+                    </div>
+                    <div className='totalCheckout'>
+                      <span>Total:</span>
+                      <span>$12999.00</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
