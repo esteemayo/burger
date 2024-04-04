be9aacfcc2cccd9d69f6d41f9fe0360ad4eb0bc5
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-04 21:13:53 +0100

    feat: create recipient store

---
+++ src/hooks/useRecipientModal.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand';
+import { produce } from 'immer';
+import { devtools } from 'zustand/middleware';
+
+import { ReciepientAction, RecipientStore } from '@/types';
+
+const INITIAL_STATE = {
+  isOpen: false,
+};
+
+export const useRecipient = create<RecipientStore & ReciepientAction>()(
+  devtools((set) => ({
+    isOpen: INITIAL_STATE.isOpen,
+    onOpen: () =>
+      set(
+        produce((state) => {
+          state.isOpen = true;
+        }),
+        false,
+        'openRecipientModal'
+      ),
+    onClose: () =>
+      set(
+        produce((state) => {
+          state.isOpen = false;
+        }),
+        false,
+        'closeRecipientModal'
+      ),
+  }))
+);
