be59f410240c5f7a0882fe3f81edad58bd67d434
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-04 17:58:45 +0100

    fix: refactor

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,4 +1,7 @@
 import Link from 'next/link';
+
+import CheckoutInfo from '@/components/checkoutInfo/CheckoutInfo';
+
 import './Checkout.scss';
 
 const Checkout = () => {
@@ -6,41 +9,7 @@ const Checkout = () => {
     <div className='checkout'>
       <div className='container'>
         <div className='checkoutWrap'>
-          <div className='checkoutInfo'>
-            <div className='checkoutInfoWrap'>
-              <div className='checkoutUserSection'>
-                <div className='checkoutInfoContainer'>
-                  <div className='checkoutInfoBox'>
-                    <h3 className='checkoutInfoHeading'>
-                      Review and place order
-                    </h3>
-                  </div>
-                  <h6 className='addressHeading'>
-                    Review/Add address and fulfill payments to complete your
-                    purchase
-                  </h6>
-                  <hr />
-                  <h6 className='recipientHeading'>Recipient information</h6>
-                  <div className='customerInfo'>
-                    <span className='customerName'>Emmanuel adebayo</span>
-                    <span className='customerPhone'>+2348136119251</span>
-                  </div>
-                  <button type='button' className='recipientBtn'>
-                    Change recipient
-                  </button>
-                  <hr />
-                  <h6 className='addressHeading'>Delivery Address</h6>
-                  <button type='button' className='deliveryBtn'>
-                    Add delivery address
-                  </button>
-                  <hr />
-                </div>
-              </div>
-              <div className='paymentSection'>
-                <button type='button'>Place your order</button>
-              </div>
-            </div>
-          </div>
+          <CheckoutInfo />
           <div className='checkoutTotal'>
             <div className='checkoutWrap'>
               <div className='checkoutDetail'>
