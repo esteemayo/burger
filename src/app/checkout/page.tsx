aa3aff08a7bdaa6ee612037ae94246ef95195a62
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-04 21:03:52 +0100

    feat: handle SEO

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,7 +1,13 @@
+import type { Metadata } from 'next';
+
 import CheckoutInfo from '@/components/checkoutInfo/CheckoutInfo';
 import CheckoutTotal from '@/components/checkoutTotal/CheckoutTotal';
 
 import './Checkout.scss';
+
+export const metadata: Metadata = {
+  title: 'Checkout page',
+};
 
 const Checkout = () => {
   return (
