c1c21b99b6ef83e4690649e4fbca235527468aa8
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-05 14:07:40 +0100

    feat: add Input fields

--- src/components/addressModal/AddressModal.tsx
+++ src/components/addressModal/AddressModal.tsx
@@ -2,7 +2,9 @@
 
 import { useCallback } from 'react';
 
+import Input from '../input/Input';
 import Modal from '../modal/Modal';
+
 import { useAddressModal } from '@/hooks/useAddressModal';
 
 import './AddressModal.scss';
@@ -15,11 +17,39 @@ const AddressModal = () => {
     console.log('clicked');
   }, []);
 
+  let bodyContent: JSX.Element;
+
+  bodyContent = (
+    <>
+      <Input
+        name='state'
+        label='State'
+        placeholder='State'
+        onChange={() => console.log('first')}
+      />
+
+      <Input
+        name='city'
+        label='City'
+        placeholder='City'
+        onChange={() => console.log('first')}
+      />
+
+      <Input
+        name='address'
+        label='Address'
+        placeholder='Address'
+        onChange={() => console.log('first')}
+      />
+    </>
+  );
+
   return (
     <Modal
       isOpen={isOpen}
       title='Add Address'
       actionLabel='Use this address'
+      body={bodyContent}
       onClose={onClose}
       onSubmit={onSubmit}
     />
