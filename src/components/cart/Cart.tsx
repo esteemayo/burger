2d65a86361ac1b6a822f3ac13c65b01b8a139cf5
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-05 17:25:15 +0100

    feat: render cart items dynamically

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,18 +1,23 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
 
+import { useCartStore } from '@/hooks/useCartStore';
+import { formatCurrency } from '@/utils/formatCurrency';
+
 import './Cart.scss';
 
 const Cart = () => {
-  const cart = [{ id: 1, name: 'Grilled chicken burger', price: 11999.0 }];
+  const products = useCartStore((store) => store.products);
 
   return (
     <aside className='productCart'>
       <div className='detailBox'>
         <div className='cardWrap'>
           <div className='cardHeading'>Your cart</div>
-          {cart.length < 1 ? (
+          {products.length < 1 ? (
             <div className='emptyCart'>
               <div className='emptyHeading'>Empty cart</div>
               <div className='emptyLink'>
@@ -21,45 +26,52 @@ const Cart = () => {
             </div>
           ) : (
             <div className='cardBody'>
-              <div className='catProduct'>
-                <div className='cardBox'>
-                  <div className='cardProductWrap'>
-                    <div className='cardProduct'>
-                      <div className='cardName'>
-                        <p>
-                          <span>1</span>
-                          Grilled chicken burger
-                        </p>
+              {products.map((product) => {
+                const { id, name, price, quantity } = product;
+                return (
+                  <div key={id} className='catProduct'>
+                    <div className='cardBox'>
+                      <div className='cardProductWrap'>
+                        <div className='cardProduct'>
+                          <div className='cardName'>
+                            <p>
+                              <span>{quantity}</span>
+                              {name}
+                            </p>
+                          </div>
+                          <div className='cardButtons'>
+                            <button type='button'>
+                              <Image
+                                src='/svg/chevron-down.svg'
+                                width={17}
+                                height={17}
+                                alt='chevron-down icon'
+                              />
+                            </button>
+                            <button type='button'>
+                              <Image
+                                src='/svg/chevron-up.svg'
+                                width={17}
+                                height={17}
+                                alt='chevron-up icon'
+                              />
+                            </button>
+                          </div>
+                          <button type='button' className='deleteCardBtn'>
+                            <RemoveShoppingCartIcon />
+                          </button>
+                          <div className='cardPrice'>
+                            {formatCurrency(price)}
+                          </div>
+                        </div>
                       </div>
-                      <div className='cardButtons'>
-                        <button type='button'>
-                          <Image
-                            src='/svg/chevron-down.svg'
-                            width={17}
-                            height={17}
-                            alt='chevron-down icon'
-                          />
-                        </button>
-                        <button type='button'>
-                          <Image
-                            src='/svg/chevron-up.svg'
-                            width={17}
-                            height={17}
-                            alt='chevron-up icon'
-                          />
-                        </button>
-                      </div>
-                      <button type='button' className='deleteCardBtn'>
-                        <RemoveShoppingCartIcon />
-                      </button>
-                      <div className='cardPrice'>$11999.0</div>
                     </div>
                   </div>
-                </div>
-              </div>
+                );
+              })}
             </div>
           )}
-          {cart.length > 0 && (
+          {products.length > 0 && (
             <div className='cardFooter'>
               <Link href='/checkout'>Proceed to Checkout</Link>
             </div>
