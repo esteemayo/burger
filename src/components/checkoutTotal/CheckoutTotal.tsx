1c42f3ea6c97857b50a75a4758099e8ffd8fb9d6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-07 02:15:09 +0100

    feat: add html markup

--- src/components/checkoutTotal/CheckoutTotal.tsx
+++ src/components/checkoutTotal/CheckoutTotal.tsx
@@ -43,72 +43,85 @@ const CheckoutTotal = () => {
               <p className='checkoutCustomer'>Emmanuel adebayo</p>
             </div>
             <div className='checkoutBody'>
-              <div className='checkoutProductBox'>
-                {products.map((product) => {
-                  const { id, name, price, quantity } = product;
-                  return (
-                    <div key={id} className='checkoutProduct'>
-                      <div className='checkoutProductName'>
-                        <Link href={`/product/${id}`}>
-                          <p>
-                            <span></span>
-                            {name}
-                          </p>
-                        </Link>
-                        <div className='checkoutQty'>
-                          <button
-                            type='button'
-                            onClick={(e) => handleDecrement(e, id)}
-                          >
-                            -
-                          </button>
-                          <span className='qty'>{quantity}</span>
-                          <button
-                            type='button'
-                            onClick={(e) => handleIncrement(e, id)}
-                          >
-                            +
-                          </button>
+              {products.length < 1 ? (
+                <div className='emptyCartWrap'>
+                  <div className='emptyCartTitle'>Empty cart</div>
+                  <div className='emptyCartLinkWrap'>
+                    <Link href='/' className='emptyCartLink'>
+                      Continue shopping
+                    </Link>
+                  </div>
+                </div>
+              ) : (
+                <>
+                  <div className='checkoutProductBox'>
+                    {products.map((product) => {
+                      const { id, name, price, quantity } = product;
+                      return (
+                        <div key={id} className='checkoutProduct'>
+                          <div className='checkoutProductName'>
+                            <Link href={`/product/${id}`}>
+                              <p>
+                                <span></span>
+                                {name}
+                              </p>
+                            </Link>
+                            <div className='checkoutQty'>
+                              <button
+                                type='button'
+                                onClick={(e) => handleDecrement(e, id)}
+                              >
+                                -
+                              </button>
+                              <span className='qty'>{quantity}</span>
+                              <button
+                                type='button'
+                                onClick={(e) => handleIncrement(e, id)}
+                              >
+                                +
+                              </button>
+                            </div>
+                          </div>
+                          <div className='checkoutDeleteWrap'>
+                            <button
+                              type='button'
+                              onClick={() => removeFromCart(id)}
+                            >
+                              <Image
+                                src='/svg/thrash.svg'
+                                width={15}
+                                height={15}
+                                alt='delete icon'
+                              />
+                            </button>
+                          </div>
+                          <div className='checkoutPrice'>
+                            <span>{formatCurrency(price)}</span>
+                          </div>
                         </div>
-                      </div>
-                      <div className='checkoutDeleteWrap'>
-                        <button
-                          type='button'
-                          onClick={() => removeFromCart(id)}
-                        >
-                          <Image
-                            src='/svg/thrash.svg'
-                            width={15}
-                            height={15}
-                            alt='delete icon'
-                          />
-                        </button>
-                      </div>
-                      <div className='checkoutPrice'>
-                        <span>{formatCurrency(price)}</span>
-                      </div>
+                      );
+                    })}
+                  </div>
+                  <div className='checkoutTotal'>
+                    <div className='checkoutTotalPrice'>
+                      <span>Items total</span>
+                      <span>{formatCurrency(totalPrice)}</span>
                     </div>
-                  );
-                })}
-              </div>
-              <div className='checkoutTotal'>
-                <div className='checkoutTotalPrice'>
-                  <span>Items total</span>
-                  <span>{formatCurrency(totalPrice)}</span>
-                </div>
-                <div className='checkoutTotalPrice'>
-                  <span>Discount</span>
-                  <span>$0.00</span>
-                </div>
-                <div className='checkoutTotalPrice'>
-                  <span>Delivery charge</span>
-                  <span>$0.00</span>
-                </div>
-              </div>
-              <div className='totalCheckout'>
-                <span>Total:</span>
-                <span>{formatCurrency(totalPrice)}</span>
-              </div>
+                    <div className='checkoutTotalPrice'>
+                      <span>Discount</span>
+                      <span>$0.00</span>
+                    </div>
+                    <div className='checkoutTotalPrice'>
+                      <span>Delivery charge</span>
+                      <span>$0.00</span>
+                    </div>
+                  </div>
+                  <div className='totalCheckout'>
+                    <span>Total:</span>
+                    <span>{formatCurrency(totalPrice)}</span>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
