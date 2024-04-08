6e5ffb6178b1881513941636ae8cac9c59a0726f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-08 17:37:07 +0100

    feat: add html markup

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,9 +1,32 @@
+import Image from 'next/image';
+
 import './Success.scss';
 
 const Success = () => {
   return (
     <div className='success'>
-      <div className='container'>Success</div>
+      <div className='container'>
+        <div className='successWrap'>
+          <div className='successBox'>
+            <div className='successIconWrap'>
+              <Image
+                src='/svg/check.svg'
+                width={30}
+                height={30}
+                alt='check icon'
+              />
+            </div>
+            <div className='successMessage'>
+              <h2 className='successHeadingSub'>Thank you</h2>
+              <h1 className='successHeadingMain'>Your order is confirmed</h1>
+              <p className='successText'>
+                We will be sending you an email confirmation to
+                eadebayo15@gmail.com shortly
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
