4fdbb5d4a4b0f553780f584fe277d1eb248b462f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-08 17:23:05 +0100

    feat: create Success page component

---
+++ src/app/success/page.tsx
@@ -0,0 +1,11 @@
+import './Success';
+
+const Success = () => {
+  return (
+    <div className='success'>
+      <div className='container'>Success</div>
+    </div>
+  );
+};
+
+export default Success;
