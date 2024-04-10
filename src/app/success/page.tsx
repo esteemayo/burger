f661d9fa2c4b650604b912514d4604def066f789
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-10 04:35:46 +0100

    feat: handle SEO

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,7 +1,13 @@
+import type { Metadata } from 'next';
+
 import SuccessInfo from '@/components/successInfo/SuccessInfo';
 import SuccessDetails from '@/components/successDetails/SuccessDetails';
 
 import './Success.scss';
+
+export const metadata: Metadata = {
+  title: 'Burger - Success page',
+};
 
 const Success = () => {
   return (
