f106d380f92651c26fbba795860aaf508ceff959
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-12 15:43:57 +0100

    feat: add getFromStorage method

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,7 @@
 export const rememberKey = 'rememberUser';
+
+export const getFromStorage = (key: string) => {
+  if (typeof window !== 'undefined') {
+    return JSON.parse(window.localStorage.getItem(key)!);
+  }
+};
