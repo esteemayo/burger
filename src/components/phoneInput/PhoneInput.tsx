9df6c56bb3a452dfd1b3b0f34af990c533e292b6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-13 17:38:08 +0100

    feat: add html markup

--- src/components/phoneInput/PhoneInput.tsx
+++ src/components/phoneInput/PhoneInput.tsx
@@ -1,7 +1,17 @@
 import './PhoneInput.scss';
 
 const PhoneInput = () => {
-  return <div className='phoneInput'>PhoneInput</div>;
+  return (
+    <div className='phoneInput'>
+      <label htmlFor='phone'>Phone number</label>
+      <div className='inputGroup'>
+        <div className='inputGroupText'>
+          +234
+          <input type='tel' name='phone' id='phone' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhoneInput;
