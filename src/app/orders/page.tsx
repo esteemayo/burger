f9d25696b312fcf1c4a704fea3a4b73c5c4b747a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-14 21:46:16 +0100

    feat: add htmlmarkup

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -3,7 +3,28 @@ import './Orders.scss';
 const Orders = () => {
   return (
     <div className='orders'>
-      <div className='container'>Orders</div>
+      <div className='container'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Order ID</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Products</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>#2059665</td>
+              <td>April 14, 2024</td>
+              <td>$199.99</td>
+              <td>Double Crunchy Chicken Burger</td>
+              <td>Preparing</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
