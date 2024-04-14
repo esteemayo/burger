afe098f971eb33e4dcd660a61395811fab84a22c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-14 21:50:32 +0100

    feat: add html markup

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,6 +1,10 @@
+import Input from '@/components/input/Input';
+
 import './Orders.scss';
 
 const Orders = () => {
+  const isAdmin = true;
+
   return (
     <div className='orders'>
       <div className='container'>
@@ -20,7 +24,19 @@ const Orders = () => {
               <td>April 14, 2024</td>
               <td>$199.99</td>
               <td>Double Crunchy Chicken Burger</td>
-              <td>Preparing</td>
+              {!isAdmin ? (
+                <td>Preparing</td>
+              ) : (
+                <td>
+                  <form onSubmit={() => console.log('submitted')}>
+                    <Input
+                      onChange={(e) => console.log('changed')}
+                      placeholder='status'
+                    />
+                    <button type='submit'>icon</button>
+                  </form>
+                </td>
+              )}
             </tr>
           </tbody>
         </table>
