92f3734debffb64670b22bdccda5808d2b221bc3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-14 21:37:50 +0100

    feat: create orders page component

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,11 @@
+import './Orders.scss';
+
+const Orders = () => {
+  return (
+    <div className='orders'>
+      <div className='container'>Orders</div>
+    </div>
+  );
+};
+
+export default Orders;
