067c2321f81b99d714df15b25405e8c3ed762e8b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-15 10:03:30 +0100

    fix: refactor

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,4 +1,4 @@
-import Input from '@/components/input/Input';
+import OrderTable from '@/components/orderTable/OrderTable';
 
 import './Orders.scss';
 
@@ -8,38 +8,7 @@ const Orders = () => {
   return (
     <div className='orders'>
       <div className='container'>
-        <table className='table'>
-          <thead>
-            <tr>
-              <th>Order ID</th>
-              <th>Date</th>
-              <th>Price</th>
-              <th>Products</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>#2059665</td>
-              <td>April 14, 2024</td>
-              <td>$199.99</td>
-              <td>Double Crunchy Chicken Burger</td>
-              {!isAdmin ? (
-                <td>Preparing</td>
-              ) : (
-                <td>
-                  <form onSubmit={() => console.log('submitted')}>
-                    <Input
-                      onChange={(e) => console.log('changed')}
-                      placeholder='status'
-                    />
-                    <button type='submit'>icon</button>
-                  </form>
-                </td>
-              )}
-            </tr>
-          </tbody>
-        </table>
+        <OrderTable isAdmin={isAdmin} />
       </div>
     </div>
   );
