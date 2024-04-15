80d9f268df98e5b4432e5eec42e33e3cf16961f7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-15 10:02:16 +0100

    feat: create OrderTable component

---
+++ src/components/orderTable/OrderTable.tsx
@@ -0,0 +1,42 @@
+import Input from '../input/Input';
+
+import './OrderTable.scss';
+
+const OrderTable = ({ isAdmin }: { isAdmin: boolean }) => {
+  return (
+    <table className='orderTable'>
+      <thead>
+        <tr>
+          <th>Order ID</th>
+          <th>Date</th>
+          <th>Price</th>
+          <th>Products</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>#2059665</td>
+          <td>April 14, 2024</td>
+          <td>$199.99</td>
+          <td>Double Crunchy Chicken Burger</td>
+          {!isAdmin ? (
+            <td>Preparing</td>
+          ) : (
+            <td>
+              <form onSubmit={() => console.log('submitted')}>
+                <Input
+                  onChange={(e) => console.log('changed')}
+                  placeholder='status'
+                />
+                <button type='submit'>icon</button>
+              </form>
+            </td>
+          )}
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default OrderTable;
