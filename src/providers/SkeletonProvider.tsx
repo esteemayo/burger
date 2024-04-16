b4c31fc78afaeb326753d5dfea5ead59b74fee37
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-16 15:48:37 +0100

    fix: modify

--- src/providers/SkeletonProvider.tsx
+++ src/providers/SkeletonProvider.tsx
@@ -1,7 +1,11 @@
 import { SkeletonTheme } from 'react-loading-skeleton';
 
 const SkeletonProvider = ({ children }: { children: React.ReactNode }) => {
-  return <SkeletonTheme>{children}</SkeletonTheme>;
+  return (
+    <SkeletonTheme baseColor='#fbfbfc' highlightColor='#f9f9f9'>
+      {children}
+    </SkeletonTheme>
+  );
 };
 
 export default SkeletonProvider;
