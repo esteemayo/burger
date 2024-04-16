89fb1a529765e84e6175e3fccfe43ad4337decc3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-16 15:42:11 +0100

    feat: add SkeletonProvider

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,21 +5,24 @@ import ClientOnly from '../clientOnly/ClientOnly';
 import ScrollTop from '../scrollTop/ScrollTop';
 import ModalProvider from '@/providers/ModalProvider';
 import CartProvider from '@/providers/CartProvider';
+import SkeletonProvider from '@/providers/SkeletonProvider';
 import ToasterProvider from '@/providers/ToasterProvider';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
       <ClientOnly>
-        <CartProvider>
-          <Navbar />
-          <ToasterProvider />
-          <ModalProvider />
-          {children}
-          <Footer />
-          <Designer />
-          <ScrollTop />
-        </CartProvider>
+        <SkeletonProvider>
+          <CartProvider>
+            <Navbar />
+            <ToasterProvider />
+            <ModalProvider />
+            {children}
+            <Footer />
+            <Designer />
+            <ScrollTop />
+          </CartProvider>
+        </SkeletonProvider>
       </ClientOnly>
     </main>
   );
