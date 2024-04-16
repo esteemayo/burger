c54bbe541a24893047334d65574c3a806304bf22
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-16 19:06:49 +0100

    feat: add RelatedCardSkeleton component

--- src/components/relatedProducts/RelatedProducts.tsx
+++ src/components/relatedProducts/RelatedProducts.tsx
@@ -1,17 +1,35 @@
-import { relatedProducts } from '@/data';
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import RelatedProduct from '../relatedProduct/RelatedProduct';
+import RelatedCardSkeleton from '../relatedCardSkeleton/RelatedCardSkeleton';
+
+import { relatedProducts } from '@/data';
 
 import './RelatedProducts.scss';
 
 const RelatedProducts = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 50000);
+  }, []);
+
   return (
     <section className='relatedProducts'>
       <div className='container'>
         <h2 className='relatedHeader'>Related products</h2>
         <div className='relatedWrap'>
-          {relatedProducts.map((product) => {
-            return <RelatedProduct key={product.id} product={product} />;
-          })}
+          {isLoading
+            ? relatedProducts.map((item) => {
+                return <RelatedCardSkeleton key={item.id} />;
+              })
+            : relatedProducts.map((product) => {
+                return <RelatedProduct key={product.id} product={product} />;
+              })}
         </div>
       </div>
     </section>
