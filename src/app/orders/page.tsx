c307f100954b17ddec548e37ca12c3a72dadd6a0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-16 14:45:34 +0100

    feat: handle SEO

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,7 +1,13 @@
+import type { Metadata } from 'next';
+
 import { orders } from '@/data';
 import OrderTable from '@/components/orderTable/OrderTable';
 
 import './Orders.scss';
+
+export const metadata: Metadata = {
+  title: 'Burger - Orders page',
+};
 
 const Orders = () => {
   const isAdmin = false;
