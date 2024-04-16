0958039f322afb389ecf2e9fdbd978fcaa076684
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-16 18:58:44 +0100

    feat: add loading skeleton

--- src/components/productLists/ProductLists.tsx
+++ src/components/productLists/ProductLists.tsx
@@ -1,14 +1,32 @@
-import { ProductListsProps } from '@/types';
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import ProductCard from '../productCard/ProductCard';
+import ProductCardSkeleton from '../productCardSkeleton/ProductCardSkeleton';
+
+import { ProductListsProps } from '@/types';
 
 import './ProductLists.scss';
 
 const ProductLists = ({ products }: ProductListsProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  }, []);
+
   return (
     <section className='productLists'>
-      {products.map((product) => {
-        return <ProductCard key={product.id} product={product} />;
-      })}
+      {isLoading
+        ? products.map((item) => {
+            return <ProductCardSkeleton key={item.id} />;
+          })
+        : products.map((product) => {
+            return <ProductCard key={product.id} product={product} />;
+          })}
     </section>
   );
 };
