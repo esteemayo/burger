84c17ba15806b28a4105708cd5e3c3ac0f2ac003
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-16 19:11:28 +0100

    feat: add html markup

--- src/components/relatedCardSkeleton/RelatedCardSkeleton.tsx
+++ src/components/relatedCardSkeleton/RelatedCardSkeleton.tsx
@@ -1,7 +1,18 @@
+import Skeleton from 'react-loading-skeleton';
+
 import './RelatedCardSkeleton.scss';
 
 const RelatedCardSkeleton = () => {
-  return <article className='relatedCardSkeleton'>RelatedCardSkeleton</article>;
+  return (
+    <article className='relatedCardSkeleton'>
+      <div className='relatedCardBox'>
+        <Skeleton width={220} height={170} />
+      </div>
+      <div className='relatedCardContent'>
+        <Skeleton count={2} />
+      </div>
+    </article>
+  );
 };
 
 export default RelatedCardSkeleton;
