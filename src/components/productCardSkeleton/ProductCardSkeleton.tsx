5646cd2e376932c2a4599a129e9f9d963a59ebf1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-16 15:10:29 +0100

    feat: create ProductCardSkeleton component

---
+++ src/components/productCardSkeleton/ProductCardSkeleton.tsx
@@ -0,0 +1,7 @@
+import './ProductCardSkeleton.scss';
+
+const ProductCardSkeleton = () => {
+  return <div className='productCardSkeleton'>ProductCardSkeleton</div>;
+};
+
+export default ProductCardSkeleton;
