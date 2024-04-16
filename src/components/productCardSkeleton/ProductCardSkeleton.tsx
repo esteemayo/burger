dc79a5b857935c724ab8a6bb5829f13262502c30
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-16 15:23:14 +0100

    feat: add html markup and skeleton loader

--- src/components/productCardSkeleton/ProductCardSkeleton.tsx
+++ src/components/productCardSkeleton/ProductCardSkeleton.tsx
@@ -4,7 +4,16 @@ import 'react-loading-skeleton/dist/skeleton.css';
 import './ProductCardSkeleton.scss';
 
 const ProductCardSkeleton = () => {
-  return <div className='productCardSkeleton'>ProductCardSkeleton</div>;
+  return (
+    <div className='productCardSkeleton'>
+      <div className='skeletonImgWrap'>
+        <Skeleton width={300} height={270} />
+      </div>
+      <div className='skeletonFooter'>
+        <Skeleton count={2} />
+      </div>
+    </div>
+  );
 };
 
 export default ProductCardSkeleton;
