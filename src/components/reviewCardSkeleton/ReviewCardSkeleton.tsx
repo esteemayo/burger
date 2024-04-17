467356c23d7d027b3c570c11b908801d78d7adcf
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-17 03:25:37 +0100

    feat: create ReviewCardSkeleton component

---
+++ src/components/reviewCardSkeleton/ReviewCardSkeleton.tsx
@@ -0,0 +1,7 @@
+import './ReviewCardSkeleton.scss';
+
+const ReviewCardSkeleton = () => {
+  return <article className='reviewCardSkeleton'>ReviewCardSkeleton</article>;
+};
+
+export default ReviewCardSkeleton;
