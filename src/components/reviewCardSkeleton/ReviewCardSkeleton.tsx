1ac89da3a72420b495979444e4eab44968428acf
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-17 03:35:43 +0100

    feat: add html markup

--- src/components/reviewCardSkeleton/ReviewCardSkeleton.tsx
+++ src/components/reviewCardSkeleton/ReviewCardSkeleton.tsx
@@ -1,7 +1,24 @@
+import Skeleton from 'react-loading-skeleton';
+
 import './ReviewCardSkeleton.scss';
 
 const ReviewCardSkeleton = () => {
-  return <article className='reviewCardSkeleton'>ReviewCardSkeleton</article>;
+  return (
+    <article className='reviewCardSkeleton'>
+      <div className='reviewCardWrap'>
+        <div className='reviewCardBox'>
+          <Skeleton />
+          <div className='reviewCardRating'>
+            <Skeleton width={75} height={15} />
+          </div>
+          <div className='reviewCardUser'>
+            <Skeleton width={165} height={27} />
+            <Skeleton />
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default ReviewCardSkeleton;
