6464c854589ab55448675d6dcaec3630e4da9ea7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-18 15:33:54 +0100

    feat: create EmptyState component

---
+++ src/components/emptyState/EmptyState.tsx
@@ -0,0 +1,11 @@
+import './EmptyState.scss';
+
+const EmptyState = () => {
+  return (
+    <div className='emptyState'>
+      <div className='container'>EmptyState</div>
+    </div>
+  );
+};
+
+export default EmptyState;
