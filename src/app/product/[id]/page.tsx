a670353f96c4839b5ffa8351f8dfe3a75649d5fb
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-18 15:37:38 +0100

    feat: add EmptyState component

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,11 +1,18 @@
 import Hero from '@/components/hero/Hero';
 import ProductReview from '@/components/productReview/ProductReview';
-import ProductInfo from '@/components/productInfo/ProductInfo';
+import EmptyState from '@/components/emptyState/EmptyState';
 import RelatedProducts from '@/components/relatedProducts/RelatedProducts';
+import ProductInfo from '@/components/productInfo/ProductInfo';
 
 import './Product.scss';
 
 const Product = () => {
+  const product = false
+
+  if (!product) {
+    return <EmptyState />
+  }
+
   return (
     <div className='product'>
       <Hero />
