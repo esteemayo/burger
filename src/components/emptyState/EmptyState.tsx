71eaa3fd58bc7a238af0be73aa3607f5bc604058
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-18 15:51:01 +0100

    feat: add html markup

--- src/components/emptyState/EmptyState.tsx
+++ src/components/emptyState/EmptyState.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image';
+
+import Heading from '../heading/Heading';
+
 import './EmptyState.scss';
 
 const EmptyState = () => {
   return (
     <div className='emptyState'>
-      <div className='container'>EmptyState</div>
+      <div className='container'>
+        <Image src='/svg/empty.svg' width={400} height={400} alt='error' />
+        <Heading
+          title='Something went wrong'
+          subtitle="We're having issues loading this page. Try again"
+          center={true}
+        />
+      </div>
     </div>
   );
 };
