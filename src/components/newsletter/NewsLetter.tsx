fb1c64c377dd625c8582b9221481eeb7b309c7a7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-19 20:32:29 +0100

    fix: modify

--- src/components/newsletter/NewsLetter.tsx
+++ src/components/newsletter/NewsLetter.tsx
@@ -1,25 +1,22 @@
 'use client';
 
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 
 import './NewsLetter.scss';
 
 const NewsLetter = () => {
-  const [email, setEmail] = useState('');
-
-  const handleEmail = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  }, []);
-
   const handleSubmit = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
 
+      const form = e.target as HTMLFormElement;
+      const input = form.elements[0];
+      const email = input.value;
+
       console.log(email);
-      setEmail('');
     },
-    [email]
+    []
   );
 
   return (
@@ -33,11 +30,7 @@ const NewsLetter = () => {
         </div>
         <div className='formWrap'>
           <form onSubmit={handleSubmit}>
-            <input
-              type='email'
-              placeholder='Your email address'
-              onChange={handleEmail}
-            />
+            <input type='email' placeholder='Your email address' />
             <button type='submit'>Subscribe</button>
           </form>
         </div>
