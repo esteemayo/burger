6cfc1d491b088af8f5af9f8f44731d74a037e32b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-20 21:08:44 +0100

    feat: create SearchClient component

---
+++ src/components/searchClient/SearchClient.tsx
@@ -0,0 +1,16 @@
+import { useSearch } from '@/hooks/useSearch';
+import ProductLists from '../productLists/ProductLists';
+import './SearchClient.scss';
+
+const SearchClient = ({ products }) => {
+  const { query } = useSearch();
+
+  return (
+    <div className='searchClient'>
+      <h3>Search results for {`"${query}"`}</h3>
+      <ProductLists products={products} />
+    </div>
+  );
+};
+
+export default SearchClient;
