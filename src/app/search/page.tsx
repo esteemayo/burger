dbd10e8e9495151f7c63d1f7a102224f33ddea70
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-20 21:05:32 +0100

    fix: modify

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,16 @@
 import { products } from '@/data';
 import ProductLists from '@/components/productLists/ProductLists';
+import { useSearch } from '@/hooks/useSearch';
 
 import './Search.scss';
 
 const Search = () => {
+  const { query } = useSearch();
+
   return (
     <div className='search'>
       <div className='container'>
-        <h3>Search results for {'query'}</h3>
+        <h3>Search results for {`"${query}"`}</h3>
         <ProductLists products={products} />
       </div>
     </div>
