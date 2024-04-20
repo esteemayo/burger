d5fdf0ad19e308f4ffe77b44c23bdfe1cfc94764
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-20 20:47:32 +0100

    feat: create a search product page

---
+++ src/app/search/page.tsx
@@ -0,0 +1,11 @@
+import './Search.scss';
+
+const Search = () => {
+  return (
+    <div className='search'>
+      <div className='container'>Search</div>
+    </div>
+  );
+};
+
+export default Search;
