50a87fc4672603e86997ec1d700b850b78b35071
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-21 16:38:07 +0100

    feat: handle no search result

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,12 +4,24 @@ import { products } from '@/data';
 import SearchClient from '@/components/searchClient/SearchClient';
 
 import './Search.scss';
+import EmptyState from '@/components/emptyState/EmptyState';
 
 export const metadata: Metadata = {
   title: 'Burger - Search page',
 };
 
 const Search = () => {
+  const empty = false;
+
+  if (!empty) {
+    return (
+      <EmptyState
+        title='No product found!'
+        subtitle='No result matches your search criteria.'
+        imgSrc='web-search'
+      />
+    );
+  }
   return (
     <div className='search'>
       <div className='container'>
