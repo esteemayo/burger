b3d9c5795cc9cd9ca4955df48e0aa70a45a73d31
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-21 17:24:27 +0100

    feat: add html markup

--- src/app/order/[id]/page.tsx
+++ src/app/order/[id]/page.tsx
@@ -1,7 +1,38 @@
 import './Order.scss';
 
 const Order = () => {
-  return <div className='order'>Order</div>;
+  return (
+    <div className='order'>
+      <div className='container'>
+        <div className='orderBox'>
+          <div className='orderWrap'>
+            <h1>Preparing your order</h1>
+            <p>
+              Arrives between <b>11:52PM - 12:02AM</b>
+            </p>
+          </div>
+          <ul className='stepper'>
+            <li className='done'>
+              <div className='item'>Order confirmed</div>
+            </li>
+            <li className='done'>
+              <div className='item'>Start production</div>
+            </li>
+            <li className='ready'>
+              <div className='item'>Quality check</div>
+            </li>
+            <li className='ready'>
+              <div className='item'>Dispatched item</div>
+            </li>
+            <li className='ready'>
+              <div className='item'>Product delivered</div>
+            </li>
+          </ul>
+          <span>Burger Inc is preparing your order.</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Order;
