3f27f0f959c23c9ee467a367a946eaf9a25f0e6e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-21 16:56:22 +0100

    fix: modify

--- src/components/emptyState/EmptyState.tsx
+++ src/components/emptyState/EmptyState.tsx
@@ -1,13 +1,13 @@
 'use client';
 
+import { useCallback } from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 import Heading from '../heading/Heading';
 import { EmptyStateProps } from '@/types';
 
 import './EmptyState.scss';
-import { useRouter } from 'next/navigation';
-import { useCallback } from 'react';
 
 const EmptyState = ({
   title,
