f83b900612c053cbaeae5d780f806f8f527701d7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-22 16:05:48 +0100

    feat: add react confetti

--- src/app/success/SuccessClient.tsx
+++ src/app/success/SuccessClient.tsx
@@ -1,11 +1,38 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+import Confetti from 'react-confetti-explosion';
+
 import SuccessInfo from '@/components/successInfo/SuccessInfo';
 import SuccessDetails from '@/components/successDetails/SuccessDetails';
 
 import './Success.scss';
 
 const SuccessClient = () => {
+  const [dimension, setDimension] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const handleSize = useCallback(() => {
+    setDimension({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleSize);
+    return () => window.removeEventListener('resize', handleSize);
+  }, [handleSize]);
+
   return (
     <div className='wrapper'>
+      <Confetti
+        width={dimension.width}
+        height={dimension.height}
+        duration={5000}
+      />
       <SuccessInfo />
       <SuccessDetails />
     </div>
