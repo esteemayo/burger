576868c60a72b0c3079b52938679c9d4579c64b9
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-22 15:11:35 +0100

    fix: refactor

--- src/app/order/[id]/page.tsx
+++ src/app/order/[id]/page.tsx
@@ -1,9 +1,9 @@
-import { excerpts } from '@/utils';
+import OrderDetail from './OrderDetail';
+import OrderStatus from './OrderStatus';
+
 import { order } from '@/data';
-import { formatCurrency } from '@/utils/formatCurrency';
 
 import './Order.scss';
-import OrderDetail from './OrderDetail';
 
 const Order = () => {
   return (
@@ -12,30 +12,7 @@ const Order = () => {
         <div className='orderBox'>
           <OrderDetail order={order} />
           <hr />
-          <div className='orderWrap'>
-            <h2>Preparing your order</h2>
-            <p>
-              Arrives between <b>11:52PM - 12:02AM</b>
-            </p>
-          </div>
-          <ul className='stepper'>
-            <li className='done'>
-              <div className='item'>Order confirmed</div>
-            </li>
-            <li className='done'>
-              <div className='item'>Start production</div>
-            </li>
-            <li className='ready'>
-              <div className='item'>Quality check</div>
-            </li>
-            <li className='ready'>
-              <div className='item'>Dispatched item</div>
-            </li>
-            <li className='ready'>
-              <div className='item'>Product delivered</div>
-            </li>
-          </ul>
-          <span className='brand'>Burger Inc is preparing your order.</span>
+          <OrderStatus />
         </div>
       </div>
     </div>
