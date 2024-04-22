8e5a673e5cacd305fdac3c0d35f83498ccac8f9f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-22 15:05:18 +0100

    fix: modify

--- src/app/order/[id]/OrderDetail.tsx
+++ src/app/order/[id]/OrderDetail.tsx
@@ -7,14 +7,14 @@ import './Order.scss';
 
 const OrderDetail = ({ order }: OrderDetailProps) => {
   return (
-    <div className='orderContainer'>
+    <div className='orderDetail'>
       <h1 className='orderHeading'>Order Detail</h1>
-      <div className='orderDetails'>
-        <div className='orderDetailItem'>
+      <div className='orderContainer'>
+        <div className='orderItem'>
           <h3>Order ID</h3>
           <span>#{order.id}</span>
         </div>
-        <div className='orderDetailItem'>
+        <div className='orderItem'>
           <h3>Ship Date</h3>
           <time dateTime={order.createdAt}>
             {new Date(order.createdAt).toLocaleString('en-us', {
@@ -24,19 +24,19 @@ const OrderDetail = ({ order }: OrderDetailProps) => {
             })}
           </time>
         </div>
-        <div className='orderDetailItem'>
+        <div className='orderItem'>
           <h3>Customer</h3>
           <span>{order.name}</span>
         </div>
-        <div className='orderDetailItem'>
+        <div className='orderItem'>
           <h3>Shipping To</h3>
           <span>{excerpts(order.address, 15)}</span>
         </div>
-        <div className='orderDetailItem'>
+        <div className='orderItem'>
           <h3>Total</h3>
           <span className='orderTotal'>{formatCurrency(order.total)}</span>
         </div>
-        <div className='orderDetailItem'>
+        <div className='orderItem'>
           <h3>Delivery Date</h3>
           <time dateTime={order.deliveryDate}>
             {new Date(order.deliveryDate).toLocaleString('en-us', {
