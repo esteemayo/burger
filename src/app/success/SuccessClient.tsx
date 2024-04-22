471d48defb428901315fd335d8d3199b90c183f0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-22 16:19:03 +0100

    fix: modify

--- src/app/success/SuccessClient.tsx
+++ src/app/success/SuccessClient.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Confetti from 'react-confetti-explosion';
+import ConfettiExplosion from 'react-confetti-explosion';
 import { useCallback, useEffect, useState } from 'react';
 
 import SuccessInfo from '@/components/successInfo/SuccessInfo';
@@ -9,6 +9,7 @@ import SuccessDetails from '@/components/successDetails/SuccessDetails';
 import './Success.scss';
 
 const SuccessClient = () => {
+  const [isExploding, setIsExploding] = useState(true);
   const [dimension, setDimension] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
@@ -26,13 +27,21 @@ const SuccessClient = () => {
     return () => window.removeEventListener('resize', handleSize);
   }, [handleSize]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsExploding(false);
+    }, 5000);
+  }, []);
+
   return (
     <div className='wrapper'>
-      <Confetti
-        width={dimension.width}
-        height={dimension.height}
-        duration={5000}
-      />
+      {isExploding && (
+        <ConfettiExplosion
+          width={dimension.width}
+          height={dimension.height}
+          duration={5000}
+        />
+      )}
       <SuccessInfo />
       <SuccessDetails />
     </div>
