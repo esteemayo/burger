91b83afb2564fb399d6d47d87ab5e8f26c65857f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-22 15:10:52 +0100

    feat: create OrderStatus component

---
+++ src/app/order/[id]/OrderStatus.tsx
@@ -0,0 +1,34 @@
+import './Order.scss';
+
+const OrderStatus = () => {
+  return (
+    <div className='orderStatus'>
+      <div className='orderWrap'>
+        <h2>Preparing your order</h2>
+        <p>
+          Arrives between <b>11:52PM - 12:02AM</b>
+        </p>
+      </div>
+      <ul className='stepper'>
+        <li className='done'>
+          <div className='item'>Order confirmed</div>
+        </li>
+        <li className='done'>
+          <div className='item'>Start production</div>
+        </li>
+        <li className='ready'>
+          <div className='item'>Quality check</div>
+        </li>
+        <li className='ready'>
+          <div className='item'>Dispatched item</div>
+        </li>
+        <li className='ready'>
+          <div className='item'>Product delivered</div>
+        </li>
+      </ul>
+      <span className='brand'>Burger Inc is preparing your order.</span>
+    </div>
+  );
+};
+
+export default OrderStatus;
