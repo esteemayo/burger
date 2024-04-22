c81085a7b2967bf72c21baac37de69393b1ecf8a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-22 14:59:45 +0100

    feat: create OrderDetail component

---
+++ src/app/order/[id]/OrderDetail.tsx
@@ -0,0 +1,49 @@
+import './Order.scss';
+
+const OrderDetail = ({ order }) => {
+  return (
+    <div className='orderContainer'>
+      <h1 className='orderHeading'>Order Detail</h1>
+      <div className='orderDetails'>
+        <div className='orderDetailItem'>
+          <h3>Order ID</h3>
+          <span>#{order.id}</span>
+        </div>
+        <div className='orderDetailItem'>
+          <h3>Ship Date</h3>
+          <time dateTime={order.createdAt}>
+            {new Date(order.createdAt).toLocaleString('en-us', {
+              month: 'long',
+              day: 'numeric',
+              year: 'numeric',
+            })}
+          </time>
+        </div>
+        <div className='orderDetailItem'>
+          <h3>Customer</h3>
+          <span>{order.name}</span>
+        </div>
+        <div className='orderDetailItem'>
+          <h3>Shipping To</h3>
+          <span>{excerpts(order.address, 15)}</span>
+        </div>
+        <div className='orderDetailItem'>
+          <h3>Total</h3>
+          <span className='orderTotal'>{formatCurrency(order.total)}</span>
+        </div>
+        <div className='orderDetailItem'>
+          <h3>Delivery Date</h3>
+          <time dateTime={order.deliveryDate}>
+            {new Date(order.deliveryDate).toLocaleString('en-us', {
+              month: 'long',
+              day: 'numeric',
+              year: 'numeric',
+            })}
+          </time>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderDetail;
