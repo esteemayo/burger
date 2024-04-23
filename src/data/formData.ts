d6e2028db6ad2f8bb7ae5299a8ba126d09f39d3d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-23 15:46:03 +0100

    feat: add gender options data items

---
+++ src/data/formData.ts
@@ -0,0 +1,7 @@
+import { GenderType } from '@/types';
+
+export const options: GenderType = [
+  { value: 'male', label: 'Male' },
+  { value: 'female', label: 'Female' },
+  { value: 'other', label: 'Other' },
+];
