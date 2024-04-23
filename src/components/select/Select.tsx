224fa6f72f94c5973f9c2ba08a5d2ac44edd0487
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-23 03:17:57 +0100

    feat: add html markup

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,7 +1,37 @@
+import ReactSelect from 'react-select';
+
 import './Select.scss';
 
 const Select = () => {
-  return <div className='select'>Select</div>;
+  const options = [
+    { value: 'male', label: 'Male' },
+    { value: 'female', label: 'Female' },
+    { value: 'other', label: 'Other' },
+  ];
+
+  return (
+    <div className='select'>
+      <ReactSelect
+        placeholder='Select your gender'
+        isClearable
+        options={options}
+        formatOptionLabel={(option) => (
+          <div className='selectOption'>
+            <span>{option.label}</span>
+          </div>
+        )}
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 5,
+          colors: {
+            ...theme.colors,
+            primary: '#00000059',
+            primary25: '#f7dbdb',
+          },
+        })}
+      />
+    </div>
+  );
 };
 
 export default Select;
