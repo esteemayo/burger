59887a7d4ad9403a43cce124543f0fd9ca714223
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-04-26 20:40:30 +0100

    fix: modify

--- src/components/offer/Offer.tsx
+++ src/components/offer/Offer.tsx
@@ -17,8 +17,8 @@ const Offer = () => {
           <h1 className='heading'>Double crunchy chicken burger</h1>
           <p className='desc'>
             Our Double crunchy chicken burger are ultra crispy and absolutely
-            bursting with flavour topped with fresh lettuce , cucumbers ,Onions
-            , our special sauce and cheese! Its an absolute crowd pleaser.
+            bursting with flavour topped with fresh lettuce, cucumbers, Onions,
+            our special sauce and cheese! Its an absolute crowd pleaser.
           </p>
           <CountDown />
           <button type='button'>Order now</button>
