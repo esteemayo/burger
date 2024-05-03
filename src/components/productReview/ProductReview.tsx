e20c04edfbc2704165d8967aa0ee614b36063e07
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-03 08:53:26 +0100

    fix: refactor

--- src/components/productReview/ProductReview.tsx
+++ src/components/productReview/ProductReview.tsx
@@ -1,11 +1,25 @@
+'use client';
+
+import { useMemo } from 'react';
+
 import Cart from '../cart/Cart';
 import Reviews from '../reviews/Reviews';
+
+import { useCartStore } from '@/hooks/useCartStore';
 
 import './ProductReview.scss';
 
 const ProductReview = () => {
+  const products = useCartStore((store) => store.products);
+
+  const productClasses = useMemo(() => {
+    return products.length < 1
+      ? 'productReview emptyProductReview'
+      : 'productReview';
+  }, [products.length]);
+
   return (
-    <section className='productReview'>
+    <section className={productClasses}>
       <div className='container'>
         <Reviews />
         <Cart />
