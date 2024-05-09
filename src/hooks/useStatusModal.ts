f5cff7206b5fd400db02a13a8cfc5a5d6bdb48c3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-09 01:44:55 +0100

    feat: add status modal store

---
+++ src/hooks/useStatusModal.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand';
+import { produce } from 'immer';
+import { devtools } from 'zustand/middleware';
+
+import { StatusAction, StatusStore } from '@/types';
+
+const INITIAL_STATE = {
+  isOpen: false,
+};
+
+export const useStatusModal = create<StatusStore & StatusAction>()(
+  devtools((set) => ({
+    isOpen: INITIAL_STATE.isOpen,
+    onOpen: () =>
+      set(
+        produce((state) => {
+          state.isOpen = true;
+        }),
+        false,
+        'openStatusModal'
+      ),
+    onClose: () =>
+      set(
+        produce((state) => {
+          state.isOpen = false;
+        }),
+        false,
+        'closeStatusModal'
+      ),
+  }))
+);
