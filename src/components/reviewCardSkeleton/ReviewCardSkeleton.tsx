7dc3440c3c6ac8afa14e36d526eebabd2a043216
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-21 01:28:26 +0100

    feat: add media query

--- src/components/reviewCardSkeleton/ReviewCardSkeleton.tsx
+++ src/components/reviewCardSkeleton/ReviewCardSkeleton.tsx
@@ -21,11 +21,28 @@ const ReviewCardSkeleton = () => {
   let width;
   let height;
 
+  if (dimension <= 300) {
+    width = 32;
+    height = 32;
+  } else if (dimension <= 429) {
+    width = 50;
+    height = 50;
+  } else if (dimension <= 600) {
+    width = 70;
+    height = 70;
+  } else if (dimension >= 1800) {
+    width = 120;
+    height = 120;
+  } else {
+    width = 87;
+    height = 87;
+  }
+
   return (
     <article className='reviewCardSkeleton'>
       <div className='reviewCardWrap'>
         <div className='reviewCardBox'>
-          <Skeleton circle width={87} height={87} />
+          <Skeleton circle width={width} height={height} />
           <div className='reviewCardRating'>
             <Skeleton width={75} height={15} />
           </div>
