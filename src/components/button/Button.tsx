bd7a47f08d9c83da76d108fc0643e7a1e85ae5cd
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-22 19:54:32 +0100

    fix: fix an error

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -8,9 +8,15 @@ const Button = ({
   type = 'button',
   className,
   children,
+  onClick,
 }: ButtonProps) => {
   return (
-    <button type={type} disabled={disabled} className={className}>
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      className={className}
+    >
       {children}
       {loading ? <Spinner /> : label}
     </button>
