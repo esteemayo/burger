febcc2cefcb11ae49607280284d96e385adfe139
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-23 20:05:43 +0100

    fix: modify

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -4,10 +4,10 @@ import { toast } from 'react-toastify';
 import logger from './logService';
 
 const devEnv = process.env.NODE_ENV !== 'production';
-const { DEV_API_URL, PROD_API_URL } = process.env;
+const { NEXT_PUBLIC_DEV_API_URL, NEXT_PUBLIC_PROD_API_URL } = process.env;
 
 const authFetch = Axios.create({
-  baseURL: devEnv ? DEV_API_URL : PROD_API_URL,
+  baseURL: devEnv ? NEXT_PUBLIC_DEV_API_URL : NEXT_PUBLIC_PROD_API_URL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
