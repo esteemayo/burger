04837d0a252a75c91d47b8dd09711802d1d6f047
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-26 17:44:57 +0100

    fix: refactor

---
+++ src/app/product/[id]/ProductClient.tsx
@@ -0,0 +1,19 @@
+import Hero from '@/components/hero/Hero';
+import ProductReview from '@/components/productReview/ProductReview';
+import ProductInfo from '@/components/productInfo/ProductInfo';
+import RelatedProducts from '@/components/relatedProducts/RelatedProducts';
+
+import './Product.scss';
+
+const ProductClient = () => {
+  return (
+    <div className='product'>
+      <Hero />
+      <ProductInfo rating={4} />
+      <ProductReview />
+      <RelatedProducts />
+    </div>
+  );
+};
+
+export default ProductClient;
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,10 +1,5 @@
-import Hero from '@/components/hero/Hero';
-import ProductReview from '@/components/productReview/ProductReview';
+import ProductClient from './ProductClient';
 import EmptyState from '@/components/emptyState/EmptyState';
-import RelatedProducts from '@/components/relatedProducts/RelatedProducts';
-import ProductInfo from '@/components/productInfo/ProductInfo';
-
-import './Product.scss';
 
 const Product = () => {
   const product = true;
@@ -19,14 +14,7 @@ const Product = () => {
     );
   }
 
-  return (
-    <div className='product'>
-      <Hero />
-      <ProductInfo rating={4} />
-      <ProductReview />
-      <RelatedProducts />
-    </div>
-  );
+  return <ProductClient />;
 };
 
 export default Product;
