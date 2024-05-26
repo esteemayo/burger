26af042c9b1bc2defbdbe953bdbe52233379dbb5
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-26 01:49:23 +0100

    feat: handle fetch single product api endpoint logic

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { prisma } from '@/utils/connect';
+
+interface IParams {
+  params: {
+    id: string;
+  };
+}
+
+export const GET = async (req: NextRequest, { params }: IParams) => {
+  const { id: productId } = params;
+
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (!product) {
+      throw new Error(`No product found with the given ID → ${productId}`);
+    }
+
+    return new NextResponse(JSON.stringify(product), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: 'Something went wrong' }),
+      { status: 500 }
+    );
+  }
+};
