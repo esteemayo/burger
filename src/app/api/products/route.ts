c64a45fca62913e72e0f25c4bb76be74d586d83c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-26 01:40:29 +0100

    feat: handle fetch all products and create new product api endpoint logic

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { prisma } from '@/utils/connect';
+
+export const GET = async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+  const isFeatured = searchParams.get('featured');
+
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        ...(isFeatured ? { isFeatured: true } : {}),
+      },
+    });
+
+    return new NextResponse(JSON.stringify(products), { status: 200 });
+  } catch (err: unknown) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: 'Something went wrong' }),
+      { status: 500 }
+    );
+  }
+};
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+
+  try {
+    const product = await prisma.product.create({
+      data: { ...body },
+    });
+
+    return new NextResponse(JSON.stringify(product), { status: 201 });
+  } catch (err: unknown) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: 'Something went wrong' }),
+      { status: 500 }
+    );
+  }
+};
