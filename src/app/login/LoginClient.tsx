b5f0b895d1be97f0e252186215eb95b347950a57
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-26 01:15:32 +0100

    fix: modify

--- src/app/login/LoginClient.tsx
+++ src/app/login/LoginClient.tsx
@@ -1,9 +1,22 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+
 import LoginForm from './LoginForm';
 import LoginImage from './LoginImage';
 
 import './Login.scss';
 
 const LoginClient = () => {
+  const router = useRouter();
+  const { status } = useSession();
+
+  if (status === 'authenticated') {
+    router.push('/');
+    return;
+  }
+
   return (
     <div className='login'>
       <div className='container'>
