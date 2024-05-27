9be779c9bca65dbaecdc2ca11bbfbe5b561d4f06
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-27 23:09:06 +0100

    feat: handle update product api endpoint logic

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 import { prisma } from '@/utils/connect';
+import { getAuthSession } from '@/utils/auth';
 
 interface IParams {
   params: {
@@ -34,4 +35,48 @@ export const GET = async (req: NextRequest, { params }: IParams) => {
       { status: 500 }
     );
   }
+};
+
+export const PATCH = async (req: NextRequest, { params }: IParams) => {
+  const { id: productId } = params;
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      const body = await req.json();
+
+      if (session.user.isAdmin) {
+        const product = await prisma.product.update({
+          where: {
+            id: productId,
+          },
+          data: { ...body },
+        });
+
+        if (!product) {
+          return new NextResponse(
+            JSON.stringify({
+              message: `No product found with the given ID → ${productId}`,
+            }),
+            { status: 400 }
+          );
+        }
+
+        return new NextResponse(JSON.stringify(product), { status: 200 });
+      }
+      return new NextResponse(
+        JSON.stringify({ message: 'You are not authorized' }),
+        { status: 403 }
+      );
+    } catch (err: unknown) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(
+    JSON.stringify({ message: 'You are not authenticated' }),
+    { status: 401 }
+  );
 };
