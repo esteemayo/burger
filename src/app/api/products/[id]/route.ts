ffcb6c9e4017221c13f21ba8188b99d9bced696d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-28 00:58:58 +0100

    feat: handle delete product api endpoint logic

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -80,3 +80,44 @@ export const PATCH = async (req: NextRequest, { params }: IParams) => {
     { status: 401 }
   );
 };
+
+export const DELETE = async (req: NextRequest, { params }: IParams) => {
+  const { id: productId } = params;
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      if (session.user.isAdmin) {
+        const product = await prisma.product.delete({
+          where: {
+            id: productId,
+          },
+        });
+
+        if (!product) {
+          return new NextResponse(
+            JSON.stringify({
+              message: `No product found with the given ID → ${productId}`,
+            }),
+            { status: 400 }
+          );
+        }
+
+        return new NextResponse(JSON.stringify(product), { status: 204 });
+      }
+      return new NextResponse(
+        JSON.stringify({ message: 'You are not authorized' }),
+        { status: 403 }
+      );
+    } catch (err: unknown) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(
+    JSON.stringify({ message: 'You are not authenticated' }),
+    { status: 401 }
+  );
+};
