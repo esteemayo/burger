d49d58109991a837e4097353ce2aeb655043340d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-27 07:23:44 +0100

    feat: add search product api endpoint

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -7,5 +7,8 @@ export const getProducts = () => http.get('/api/products');
 export const getFeaturedProducts = () =>
   http.get('/api/products?featured=true');
 
+export const searchProducts = (searchQuery: string) =>
+  http.get(`/api/products/search?q=${searchQuery}`);
+
 export const getProduct = (productId: string) =>
   http.get(`${apiEndpoint}/${productId}`);
