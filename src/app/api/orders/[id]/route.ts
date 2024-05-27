1dcc3d7dda04024fda358c845eb5852e849af8fe
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-27 22:34:46 +0100

    feat: handle delete order api endpoint logic

--- src/app/api/orders/[id]/route.ts
+++ src/app/api/orders/[id]/route.ts
@@ -107,3 +107,44 @@ export const PATCH = async (req: NextRequest, { params }: IParams) => {
     { status: 401 }
   );
 };
+
+export const DELETE = async (req: NextRequest, { params }: IParams) => {
+  const { id: orderId } = params;
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      if (session.user.isAdmin) {
+        const order = await prisma.order.delete({
+          where: {
+            id: orderId,
+          },
+        });
+
+        if (!order) {
+          return new NextResponse(
+            JSON.stringify({
+              message: `No order found with the given ID → ${orderId}`,
+            }),
+            { status: 400 }
+          );
+        }
+
+        return new NextResponse(JSON.stringify(order), { status: 204 });
+      }
+      return new NextResponse(
+        JSON.stringify({ message: 'You are not authorized' }),
+        { status: 403 }
+      );
+    } catch (err: unknown) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(
+    JSON.stringify({ message: 'You are not authenticated' }),
+    { status: 401 }
+  );
+};
