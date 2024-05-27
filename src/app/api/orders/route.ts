93d04cc65e8cd108abfce56d147c1d97d9890a8f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-27 22:07:16 +0100

    feat: handle create new order api endpoint logic

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 import { prisma } from '@/utils/connect';
 import { getAuthSession } from '@/utils/auth';
@@ -21,6 +21,39 @@ export const GET = async () => {
       });
 
       return new NextResponse(JSON.stringify(orders), { status: 200 });
+    } catch (err: unknown) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(
+    JSON.stringify({ message: 'You are not authenticated' }),
+    { status: 401 }
+  );
+};
+
+export const POST = async (req: NextRequest) => {
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      if (!session.user.isAdmin) {
+        const body = await req.json();
+
+        const order = await prisma.order.create({
+          data: { ...body },
+        });
+
+        return new NextResponse(JSON.stringify(order), { status: 201 });
+      }
+      return new NextResponse(
+        JSON.stringify({
+          message: 'You are not authorized to perform this action',
+        }),
+        { status: 403 }
+      );
     } catch (err: unknown) {
       return new NextResponse(
         JSON.stringify({ message: 'Something went wrong' }),
