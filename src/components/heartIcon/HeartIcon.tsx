f19ea3b4344144f66f7785cde599fb31eda2d3d6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-28 19:00:07 +0100

    feat: create HeartIcon component

---
+++ src/components/heartIcon/HeartIcon.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+interface HeartIconProps {
+  isFavorite: boolean;
+}
+
+const HeartIcon = ({ isFavorite }: HeartIconProps) => {
+  return isFavorite ? (
+    <Image src='/svg/heart-2.svg' width={20} height={20} alt='heart icon' />
+  ) : (
+    <Image src='/svg/heart-1.svg' width={20} height={20} alt='heart icon' />
+  );
+};
+
+export default HeartIcon;
