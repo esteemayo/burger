f56fd294bf1a9fd5d2753d37beb4f0c0a4c70207
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-28 18:42:22 +0100

    feat: handle like product functionality

--- src/hooks/useFavorite.tsx
+++ src/hooks/useFavorite.tsx
@@ -1,11 +1,51 @@
-import { CurrentUserType } from '@/types';
+'use client';
 
-const useFavorite = (
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import { useCallback, useMemo } from 'react';
+
+import { CurrentUserType, IFavorite } from '@/types';
+import { likeProduct } from '@/services/productService';
+
+const useFavorite: IFavorite = (
   actionId: string,
-  currentUser: CurrentUserType | null,
+  currentUser: CurrentUserType | undefined,
   likes: string[]
 ) => {
-  return <div>useFavorite</div>;
+  const router = useRouter();
+
+  const hasFavorited = useMemo(() => {
+    const product = likes || [];
+    const userId = currentUser?.id;
+
+    const list = product.includes(userId!);
+    return !!list;
+  }, [currentUser, likes]);
+
+  const toggleFavorite = useCallback(
+    async (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      if (!currentUser) {
+        router.push('/login');
+        return;
+      }
+
+      try {
+        const { data } = await likeProduct(actionId);
+        console.log(data);
+      } catch (err: unknown) {
+        console.log(err);
+        toast.error('Something went wrong');
+      }
+    },
+    [actionId, currentUser, router]
+  );
+
+  return {
+    hasFavorited,
+    toggleFavorite,
+  };
 };
 
 export default useFavorite;
