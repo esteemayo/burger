7965a5c3c1ff020e75136767f588f8c328b5025c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-29 11:37:01 +0100

    feat: handle delete review api endpoint logic

--- src/app/api/reviews/[id]/route.ts
+++ src/app/api/reviews/[id]/route.ts
@@ -93,3 +93,50 @@ export const PATCH = async (req: NextRequest, { params }: IParams) => {
     { status: 401 }
   );
 };
+
+export const DELETE = async (req: NextRequest, { params }: IParams) => {
+  const { id: reviewId } = params;
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      let review = await prisma.review.findUnique({
+        where: {
+          id: reviewId,
+        },
+      });
+
+      if (!review) {
+        return new NextResponse(
+          JSON.stringify({
+            message: `No review found with the given ID → ${reviewId}`,
+          }),
+          { status: 400 }
+        );
+      }
+
+      if (session.user.isAdmin || session.user.id === review.userId) {
+        review = await prisma.review.delete({
+          where: {
+            id: reviewId,
+          },
+        });
+
+        return new NextResponse(JSON.stringify(review), { status: 204 });
+      }
+      return new NextResponse(
+        JSON.stringify({ message: 'You are not authorized' }),
+        { status: 403 }
+      );
+    } catch (err: unknown) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(
+    JSON.stringify({ message: 'You are not authenticated' }),
+    { status: 401 }
+  );
+};
