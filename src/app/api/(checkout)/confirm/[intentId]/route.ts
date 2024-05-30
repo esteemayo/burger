8c385906fa1e35b20b5290fb5d0dbda7ee605c43
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-05-30 17:20:30 +0100

    feat: handle confirm order api endpoint logic

---
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { prisma } from '@/utils/connect';
+import { getAuthSession } from '@/utils/auth';
+
+interface IParams {
+  params: {
+    intentId: string;
+  };
+}
+
+export const PATCH = async (req: NextRequest, { params }: IParams) => {
+  const { intentId } = params;
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      const order = await prisma.order.update({
+        where: {
+          intent_id: intentId,
+        },
+        data: {
+          status: 'preparing',
+        },
+      });
+
+      return new NextResponse(JSON.stringify(order), { status: 200 });
+    } catch (err: unknown) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(
+    JSON.stringify({ message: 'You are not authenticated' }),
+    { status: 401 }
+  );
+};
