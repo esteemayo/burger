98b7a2ef08f77f20a506e35a6e89bbaee9cb7446
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-01 08:52:20 +0100

    feat: create payment component page

--- src/app/payment/[id]/page.tsx
+++ src/app/payment/[id]/page.tsx
@@ -1,9 +1,27 @@
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+import CheckoutForm from '@/components/checkoutForm/CheckoutForm';
+
 import './Payment.scss';
 
-const Payment  = () => {
+const stripePromise = loadStripe(
+  'pk_test_51PMC1YLjKbhA4Yjg89VxYW1rFYaqbOZ3TQqbc4AtBY0PMnQzwJAad7eOBO1HWFbSgrHY1E0MpqQvx94jrZhvx2Bq00jwNTSLcT'
+);
+
+const Payment = () => {
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: '{{CLIENT_SECRET}}',
+  };
+
   return (
     <div className='payment'>
-      <div className='container'>Payment</div>
+      <div className='container'>
+        <Elements stripe={stripePromise} options={options}>
+          <CheckoutForm />
+        </Elements>
+      </div>
     </div>
   );
 };
