e98ed97b5dfedc69c67ccb055da00d1ff421838b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-02 19:12:01 +0100

    feat: add create order api url endpoint

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -6,3 +6,6 @@ export const getOrders = () => http.get(apiEndpoint);
 
 export const getOrder = (orderId: string) =>
   http.get(`${apiEndpoint}/${orderId}`);
+
+export const createOrder = <T extends object>(data: T) =>
+  http.post('/api/orders', data);
