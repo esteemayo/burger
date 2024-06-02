018101c16f09c08a4eebcb5b6e3ae5502352ff30
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-02 19:10:40 +0100

    feat: add fetch order api url endpoint

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -3,3 +3,6 @@ import http from './httpService';
 const apiEndpoint = '/orders';
 
 export const getOrders = () => http.get(apiEndpoint);
+
+export const getOrder = (orderId: string) =>
+  http.get(`${apiEndpoint}/${orderId}`);
