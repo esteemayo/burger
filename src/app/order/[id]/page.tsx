764b64985cbed1f04cf82a00a9b99b9fdf4558ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-05 21:46:55 +0100

    fix: refactor

--- src/app/order/[id]/page.tsx
+++ src/app/order/[id]/page.tsx
@@ -1,28 +1,13 @@
 import type { Metadata } from 'next';
 
-import OrderDetail from './OrderDetail';
-import OrderStatus from './OrderStatus';
-
-import { order } from '@/data';
-
-import './Order.scss';
+import OrderClient from './OrderClient';
 
 export const metadata: Metadata = {
   title: 'Burger - Order page',
 };
 
 const Order = () => {
-  return (
-    <div className='order'>
-      <div className='container'>
-        <div className='orderBox'>
-          <OrderDetail order={order} />
-          <hr />
-          <OrderStatus />
-        </div>
-      </div>
-    </div>
-  );
+  return <OrderClient />;
 };
 
 export default Order;
