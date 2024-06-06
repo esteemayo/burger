e8517fe8011cf359cdaa0ae9c144489170dc014f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-06 23:05:42 +0100

    feat: add api url to update user data

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,3 +1,6 @@
 import http from './httpService';
 
 export const getUser = (userId: string) => http.get(`/api/users/${userId}`);
+
+export const updateUserData = <T extends object>(userId: string, data: T) =>
+  http.patch(`/api/users/${userId}`, data);
