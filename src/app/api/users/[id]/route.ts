53e71113a49534a03679de7ccf132b0c57e21a43
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-06 18:24:27 +0100

    feat: handle update user data api endpoint logic

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { NextRequest, NextResponse } from 'next/server';
 
 import { prisma } from '@/utils/connect';
@@ -36,6 +37,68 @@ export const GET = async (req: NextRequest, { params }: IParams) => {
       JSON.stringify({ message: 'You are not authorized' }),
       { status: 403 }
     );
+  }
+  return new NextResponse(
+    JSON.stringify({ message: 'You are not authenticated' }),
+    { status: 401 }
+  );
+};
+
+export const PATCH = async (req: NextRequest, { params }: IParams) => {
+  const { id: userId } = params;
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      const body = await req.json();
+
+      if (session.user.isAdmin || session.user.id === userId) {
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        });
+
+        if (!user) {
+          return new NextResponse(
+            JSON.stringify({
+              message: `No user found with the given ID → ${userId}`,
+            }),
+            { status: 400 }
+          );
+        }
+
+        const filterBody = _.pick(body, [
+          'name',
+          'username',
+          'email',
+          'phone',
+          'state',
+          'city',
+          'street',
+          'address',
+          'image',
+        ]);
+
+        const updatedUser = await prisma.user.update({
+          where: {
+            id: userId,
+          },
+          data: { ...filterBody },
+        });
+
+        return new NextResponse(JSON.stringify(updatedUser), { status: 200 });
+      }
+      return new NextResponse(
+        JSON.stringify({ message: 'You are not authorized' }),
+        { status: 403 }
+      );
+    } catch (err: unknown) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      );
+    }
   }
   return new NextResponse(
     JSON.stringify({ message: 'You are not authenticated' }),
