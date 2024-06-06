effb71a310c6e7d40b0e57228c9f9657c8fb1e52
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-06 18:28:31 +0100

    feat: handle delete user account api endpoint logic

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -112,3 +112,50 @@ export const PATCH = async (req: NextRequest, { params }: IParams) => {
     { status: 401 }
   );
 };
+
+export const DELETE = async (req: NextRequest, { params }: IParams) => {
+  const { id: userId } = params;
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      if (session.user.isAdmin || session.user.id === userId) {
+        let user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        });
+
+        if (!user) {
+          return new NextResponse(
+            JSON.stringify({
+              message: `No user found with the given ID → ${userId}`,
+            }),
+            { status: 400 }
+          );
+        }
+
+        user = await prisma.user.delete({
+          where: {
+            id: userId,
+          },
+        });
+
+        return new NextResponse(JSON.stringify(user), { status: 204 });
+      }
+      return new NextResponse(
+        JSON.stringify({ message: 'You are not authorized' }),
+        { status: 403 }
+      );
+    } catch (err: unknown) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(
+    JSON.stringify({ message: 'You are not authenticated' }),
+    { status: 401 }
+  );
+};
