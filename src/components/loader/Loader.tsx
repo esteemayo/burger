729fc68f9cffb923f3e4b7e0b065856f95fac917
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-07 21:02:06 +0100

    feat: add loading spinner

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,9 +1,13 @@
+import { ClipLoader } from 'react-spinners';
+
 import './Loader.scss';
 
 const Loader = () => {
   return (
     <div className='loader'>
-      <div className='container'>Loader</div>
+      <div className='container'>
+        <ClipLoader size={100} color='#ff0018' />
+      </div>
     </div>
   );
 };
