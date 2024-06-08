543cdd147e72a5a48d79de95781c4ca91681d3c9
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-08 13:10:48 +0100

    fix: refactor

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,13 +1,6 @@
-'use client';
-
-import Link from 'next/link';
-import Image from 'next/image';
-
-import { formatDate } from '@/utils/formatDate';
-import { excerpts } from '@/utils';
-import { formatCurrency } from '@/utils/formatCurrency';
-
 import TableHead from './TableHead';
+import TableBody from './TableBody';
+
 import { TableProps } from '@/types';
 
 import './Table.scss';
@@ -23,68 +16,14 @@ const Table = ({
   return (
     <table className='table'>
       <TableHead />
-      <tbody>
-        {data?.map((item) => {
-          const { id, price, status, products, createdAt } = item;
-          return (
-            <tr
-              key={id}
-              className={`${
-                status !== 'delivered' ? 'orderStatus' : 'orderDelivered'
-              }`}
-            >
-              <td>
-                <Link href={`/order/${encodeURIComponent(id)}`}>
-                  #{excerpts(id, 10)}
-                </Link>
-              </td>
-              <td>
-                <time dateTime={createdAt}>{formatDate(createdAt)}</time>
-              </td>
-              <td>{formatCurrency(price)}</td>
-              <td className='orderName'>
-                {products?.map((product) => {
-                  return (
-                    <span key={product.id}>
-                      {product.name} <br />
-                    </span>
-                  );
-                })}
-              </td>
-              {!isAdmin ? (
-                <td>{orderStatus(status)}</td>
-              ) : (
-                <td>
-                  <form onSubmit={onSubmit}>
-                    <input type='text' placeholder={orderStatus(status)} />
-                    <button type='submit'>
-                      <Image
-                        src='/img/edit.png'
-                        width={20}
-                        height={20}
-                        alt='edit icon'
-                      />
-                    </button>
-                  </form>
-                  {dimension <= 768 && (
-                    <div className='statusContainer'>
-                      {orderStatus(status)}
-                      <button type='button' onClick={(e) => onClick(e, item)}>
-                        <Image
-                          src='/svg/edit.svg'
-                          width={20}
-                          height={20}
-                          alt='edit icon'
-                        />
-                      </button>
-                    </div>
-                  )}
-                </td>
-              )}
-            </tr>
-          );
-        })}
-      </tbody>
+      <TableBody
+        data={data}
+        isAdmin={isAdmin}
+        dimension={dimension}
+        orderStatus={orderStatus}
+        onClick={onClick}
+        onSubmit={onSubmit}
+      />
     </table>
   );
 };
