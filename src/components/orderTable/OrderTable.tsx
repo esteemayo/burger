7a6a9e0310886fcec50456c30da30616d28e2e59
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-08 12:57:32 +0100

    fix: refactor

--- src/components/orderTable/OrderTable.tsx
+++ src/components/orderTable/OrderTable.tsx
@@ -1,18 +1,12 @@
 'use client';
 
 import { toast } from 'react-toastify';
-import Link from 'next/link';
 import { useCallback, useEffect, useState } from 'react';
-import Image from 'next/image';
-
-import { formatDate } from '@/utils/formatDate';
-import { excerpts } from '@/utils';
-import { formatCurrency } from '@/utils/formatCurrency';
 
 import { OrderItem, OrderTableProps } from '@/types';
 import { useStatusModal } from '@/hooks/useStatusModal';
 
-import './OrderTable.scss';
+import Table from '../table/Table';
 
 const OrderTable = ({ isAdmin, data }: OrderTableProps) => {
   const onOpen = useStatusModal((store) => store.onOpen);
@@ -60,82 +54,14 @@ const OrderTable = ({ isAdmin, data }: OrderTableProps) => {
   }, [handleDimension]);
 
   return (
-    <table className='orderTable'>
-      <thead>
-        <tr>
-          <th>Order ID</th>
-          <th>Date</th>
-          <th>Price</th>
-          <th>Products</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data?.map((item) => {
-          const { id, price, status, products, createdAt } = item;
-          return (
-            <tr
-              key={id}
-              className={`${
-                status !== 'delivered' ? 'orderStatus' : 'orderDelivered'
-              }`}
-            >
-              <td>
-                <Link href={`/order/${encodeURIComponent(id)}`}>
-                  #{excerpts(id, 10)}
-                </Link>
-              </td>
-              <td>
-                <time dateTime={createdAt}>{formatDate(createdAt)}</time>
-              </td>
-              <td>{formatCurrency(price)}</td>
-              <td className='orderName'>
-                {products?.map((product) => {
-                  return (
-                    <span key={product.id}>
-                      {product.name} <br />
-                    </span>
-                  );
-                })}
-              </td>
-              {!isAdmin ? (
-                <td>{orderStatus(status)}</td>
-              ) : (
-                <td>
-                  <form onSubmit={handleSubmit}>
-                    <input type='text' placeholder={orderStatus(status)} />
-                    <button type='submit'>
-                      <Image
-                        src='/img/edit.png'
-                        width={20}
-                        height={20}
-                        alt='edit icon'
-                      />
-                    </button>
-                  </form>
-                  {dimension <= 768 && (
-                    <div className='statusContainer'>
-                      {orderStatus(status)}
-                      <button
-                        type='button'
-                        onClick={(e) => handleClick(e, item)}
-                      >
-                        <Image
-                          src='/svg/edit.svg'
-                          width={20}
-                          height={20}
-                          alt='edit icon'
-                        />
-                      </button>
-                    </div>
-                  )}
-                </td>
-              )}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <Table
+      data={data}
+      isAdmin={isAdmin}
+      dimension={dimension}
+      orderStatus={orderStatus}
+      onClick={handleClick}
+      onSubmit={handleSubmit}
+    />
   );
 };
 
