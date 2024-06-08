4ba7ec459b9e6f0bae58ad7880adda888977f1aa
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-08 13:02:41 +0100

    feat: create TableHead component

---
+++ src/components/table/TableHead.tsx
@@ -0,0 +1,17 @@
+import './Table.scss';
+
+const TableHead = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Order ID</th>
+        <th>Date</th>
+        <th>Price</th>
+        <th>Products</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+  );
+};
+
+export default TableHead;
