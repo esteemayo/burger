623929e9e1b3ae05a54da351ca7e2201aa607171
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-09 23:38:05 +0100

    fix: modify

--- src/components/table/TableBody.tsx
+++ src/components/table/TableBody.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useCallback } from 'react';
 
 import { formatDate } from '@/utils/formatDate';
