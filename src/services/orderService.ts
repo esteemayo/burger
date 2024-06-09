3c63eaf0b3a642d8d5898e10720fbb880e93453d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-09 17:55:16 +0100

    fix: modify

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -11,7 +11,7 @@ export const createOrder = <T extends object>(data: T) =>
   http.post('/api/orders', data);
 
 export const updateOrder = <T extends object>(orderId: string, data: T) =>
-  http.patch(`${apiEndpoint}/${orderId}`, data);
+  http.patch(`/api/orders/${orderId}`, data);
 
 export const updatePaymentIntent = (paymentIntent: string | null) =>
   http.patch(`${apiEndpoint}/confirm/${paymentIntent}`);
