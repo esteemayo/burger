06a1dd6e4c7241a84de8b14cb04d4b2d5c49d553
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-13 17:20:15 +0100

    feat: handle update current user password endpoint logic

---
+++ src/app/api/auth/update-my-password/[id]/route.ts
@@ -0,0 +1,73 @@
+import bcrypt from 'bcryptjs';
+import _ from 'lodash';
+import { NextRequest, NextResponse } from 'next/server';
+
+import { prisma } from '@/utils/connect';
+import { getAuthSession } from '@/utils/auth';
+
+interface IParams {
+  params: {
+    id: string;
+  };
+}
+
+export const PATCH = async (req: NextRequest, { params }: IParams) => {
+  const { id: userId } = params;
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      const body = await req.json();
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+      });
+
+      if (!user) {
+        return new NextResponse(
+          JSON.stringify({
+            message: `No user found with the given ID → ${userId}`,
+          }),
+          { status: 400 }
+        );
+      }
+
+      const updPassword = _.pick(body, ['password', 'confirmPssword']);
+      const comparePassword = await bcrypt.compare(
+        updPassword.password,
+        user?.password!
+      );
+
+      if (!comparePassword) {
+        return new NextResponse(
+          JSON.stringify({
+            message: 'Your password is wrong',
+          }),
+          { status: 401 }
+        );
+      }
+
+      if (session.user.isAdmin || session.user.id === userId) {
+        const updatedUser = await prisma.user.update({
+          where: {
+            id: userId,
+          },
+          data: { ...updPassword },
+        });
+
+        return new NextResponse(JSON.stringify(updatedUser), { status: 200 });
+      }
+    } catch (err: unknown) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(
+    JSON.stringify({ message: 'You are not authenticated' }),
+    { status: 401 }
+  );
+};
