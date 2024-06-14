328222a7bc360168a5a4b6085cb9536c32459b75
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-14 11:23:36 +0100

    feat: add update current user password api url

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,3 +4,8 @@ const apiEndpoint = '/auth';
 
 export const registerUser = <T extends object>(credentials: T) =>
   http.post(`/api/auth/register`, credentials);
+
+export const updatePassword = <T extends object>(
+  userId: string,
+  credentials: T
+) => http.patch(`/api/auth/update-my-password/${userId}`, credentials);
