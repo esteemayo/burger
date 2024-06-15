1c243328b2b9850b253b52adfbc807f6bca1ea55
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-15 21:58:14 +0100

    feat: handle delete current user account

---
+++ src/app/api/users/delete-me/[id]/route.ts
@@ -0,0 +1,57 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { prisma } from '@/utils/connect';
+import { getAuthSession } from '@/utils/auth';
+
+interface IParams {
+  params: {
+    id: string;
+  };
+}
+
+export const DELETE = async (req: NextRequest, { params }: IParams) => {
+  const { id: userId } = params;
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+      });
+
+      if (!user) {
+        return new NextResponse(
+          JSON.stringify({
+            message: `No user found with the given ID → ${userId}`,
+          }),
+          { status: 400 }
+        );
+      }
+
+      if (session.user.isAdmin || session.user.id === userId) {
+        await prisma.user.delete({
+          where: {
+            id: userId,
+          },
+        });
+
+        return new NextResponse(JSON.stringify(user), { status: 204 });
+      }
+      return new NextResponse(
+        JSON.stringify({ message: 'You are not authorized' }),
+        { status: 403 }
+      );
+    } catch (err: unknown) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(
+    JSON.stringify({ message: 'You are not authenticated' }),
+    { status: 401 }
+  );
+};
