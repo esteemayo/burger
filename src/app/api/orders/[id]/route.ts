ed16e12d2c9d711255a45c39382fbb4777072d38
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-15 22:09:46 +0100

    fix: modify

--- src/app/api/orders/[id]/route.ts
+++ src/app/api/orders/[id]/route.ts
@@ -85,7 +85,7 @@ export const PATCH = async (req: NextRequest, { params }: IParams) => {
             JSON.stringify({
               message: `No order found with the given ID → ${orderId}`,
             }),
-            { status: 400 }
+            { status: 404 }
           );
         }
 
