67a212367792593d13cb16b8ce6600f5a79813e7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-17 14:22:58 +0100

    feat: handle delete current user functionality

--- src/components/accountModal/AccountModal.tsx
+++ src/components/accountModal/AccountModal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
+import { useSession } from 'next-auth/react';
 
 import Modal from '../modal/Modal';
 import DeleteAccount from '../deleteAccount/DeleteAccount';
@@ -8,15 +9,31 @@ import DeleteAccount from '../deleteAccount/DeleteAccount';
 import { useAccountModal } from '@/hooks/useAccountModal';
 
 import './AccountModal.scss';
+import { deleteUser } from '@/services/userService';
 
 const AccountModal = () => {
+  const { data: session } = useSession();
+
   const isOpen = useAccountModal((store) => store.isOpen);
   const onClose = useAccountModal((store) => store.onClose);
 
-  const onSubmit = useCallback(() => {
-    console.log('Account deactivated!');
-    onClose();
-  }, [onClose]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = useCallback(async () => {
+    setIsLoading(true);
+
+    try {
+      const userId = session?.user.id;
+
+      const { data } = await deleteUser(userId as string);
+      console.log('Account deactivated!', data);
+      onClose();
+    } catch (err: unknown) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [onClose, session]);
 
   let bodyContent: JSX.Element;
 
@@ -26,6 +43,8 @@ const AccountModal = () => {
     <Modal
       isOpen={isOpen}
       title='Delete Account'
+      loading={isLoading}
+      disabled={isLoading}
       actionLabel='Confirm Deletion'
       body={bodyContent}
       onClose={onClose}
