d8014d0d2676278b7756f0bdbdc03e4cbfc63422
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-19 21:13:47 +0100

    fix: modify

--- src/components/userMenu/UserMenu.tsx
+++ src/components/userMenu/UserMenu.tsx
@@ -4,7 +4,6 @@ import { useCallback } from 'react';
 import Link from 'next/link';
 import { signOut } from 'next-auth/react';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 
 import { UserMenuProps } from '@/types';
 
