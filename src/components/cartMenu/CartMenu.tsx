f1f5acf476b36a0a1735b5929147d52b8be25349
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-20 19:41:31 +0100

    fix: refactor

--- src/components/cartMenu/CartMenu.tsx
+++ src/components/cartMenu/CartMenu.tsx
@@ -11,6 +11,7 @@ import { CartMenuProps } from '@/types';
 import { formatCurrency } from '@/utils/formatCurrency';
 
 import './CartMenu.scss';
+import CartMenuItem from '../cartMenuItem/CartMenuItem';
 
 const CartMenu = ({
   products,
@@ -47,40 +48,14 @@ const CartMenu = ({
           <div className='cardHeader'>Your cart</div>
           <div className='cardProductBox'>
             {products.slice(0, 3).map((productItem) => {
-              const { id, name, price, quantity } = productItem;
               return (
-                <div key={id} className='productBox'>
-                  <span className='quantity'>{quantity}</span>
-                  <div className='cardItem' onClick={() => productUrl(id)}>
-                    {name}
-                  </div>
-                  <div className='cardButtons'>
-                    <button type='button' onClick={(e) => onDecrement(e, id)}>
-                      <Image
-                        src='/svg/chevron-down.svg'
-                        width={17}
-                        height={17}
-                        alt='chevron-down icon'
-                      />
-                    </button>
-                    <button
-                      type='button'
-                      disabled={quantity >= 10}
-                      onClick={(e) => onIncrement(e, id)}
-                    >
-                      <Image
-                        src='/svg/chevron-up.svg'
-                        width={17}
-                        height={17}
-                        alt='chevron-up icon'
-                      />
-                    </button>
-                  </div>
-                  <div className='deleteBtn' onClick={() => onRemove(id)}>
-                    <RemoveShoppingCartIcon />
-                  </div>
-                  <span className='price'>{formatCurrency(price)}</span>
-                </div>
+                <CartMenuItem
+                  key={productItem.id}
+                  onIncrement={onIncrement}
+                  onDecrement={onDecrement}
+                  onRemove={onRemove}
+                  {...productItem}
+                />
               );
             })}
           </div>
