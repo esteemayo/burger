f273a4b349c8718f4f12f4a2f909ca552fcf92e1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-24 21:28:41 +0100

    feat: create CheckoutProducts component

---
+++ src/components/checkoutProducts/CheckoutProducts.tsx
@@ -0,0 +1,48 @@
+import { formatCurrency } from '@/utils/formatCurrency';
+import CheckoutProduct from '../checkoutProduct/CheckoutProduct';
+
+const CheckoutProducts = ({
+  products,
+  totalPrice,
+  onIncrement,
+  onDecrement,
+  onRemove,
+}) => {
+  return (
+    <div className='checkoutProducts'>
+      <div className='checkoutProductBox'>
+        {products.map((product) => {
+          return (
+            <CheckoutProduct
+              key={product.id}
+              onIncrement={onIncrement}
+              onDecrement={onDecrement}
+              onRemove={onRemove}
+              {...product}
+            />
+          );
+        })}
+      </div>
+      <div className='checkoutTotal'>
+        <div className='checkoutTotalPrice'>
+          <span>Items total</span>
+          <span>{formatCurrency(totalPrice)}</span>
+        </div>
+        <div className='checkoutTotalPrice'>
+          <span>Discount</span>
+          <span>$0.00</span>
+        </div>
+        <div className='checkoutTotalPrice'>
+          <span>Delivery charge</span>
+          <span>$0.00</span>
+        </div>
+      </div>
+      <div className='totalCheckout'>
+        <span>Total:</span>
+        <span>{formatCurrency(totalPrice)}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutProducts;
