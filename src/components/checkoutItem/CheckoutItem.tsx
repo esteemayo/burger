a3794a65b5655995d85c77fadb834747d4fe4bff
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-24 21:03:49 +0100

    feat: create CheckoutItem component

---
+++ src/components/checkoutItem/CheckoutItem.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { formatCurrency } from '@/utils/formatCurrency';
+
+import './CheckoutItem.scss';
+
+const CheckoutItem = ({
+  id,
+  name,
+  price,
+  quantity,
+  onIncrement,
+  onDecrement,
+  onRemove,
+}) => {
+  return (
+    <article className='checkoutProduct'>
+      <div className='checkoutProductName'>
+        <Link href={`/product/${id}`}>
+          <p>
+            <span></span>
+            {name}
+          </p>
+        </Link>
+        <div className='checkoutQty'>
+          <button type='button' onClick={(e) => onDecrement(e, id)}>
+            -
+          </button>
+          <span className='qty'>{quantity}</span>
+          <button
+            type='button'
+            disabled={quantity >= 10}
+            onClick={(e) => onIncrement(e, id)}
+          >
+            +
+          </button>
+        </div>
+      </div>
+      <div className='checkoutDeleteWrap'>
+        <button type='button' onClick={() => onRemove(id)}>
+          <Image
+            src='/svg/thrash.svg'
+            width={15}
+            height={15}
+            alt='delete icon'
+          />
+        </button>
+      </div>
+      <div className='checkoutPrice'>
+        <span>{formatCurrency(price)}</span>
+      </div>
+    </article>
+  );
+};
+
+export default CheckoutItem;
