faa3d7cc9573be1ed9f13929fbe68c68a54a1eb7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-27 17:37:12 +0100

    fix: fix a type error

--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
-export const uploadImage = (file: File | undefined) =>
+export const uploadImage = (data: FormData) =>
   Axios.post(
     'https://api.cloudinary.com/v1_1/learnhowtocode/image/upload',
-    file
+    data
   );
