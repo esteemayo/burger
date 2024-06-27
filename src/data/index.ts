a102e473990343ac04507be729edb1a1bb2c25a0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-27 01:00:34 +0100

    fix: modify

--- src/data/index.ts
+++ src/data/index.ts
@@ -433,7 +433,7 @@ export const products: CardMenu = [
     id: 18,
     name: 'A Double Chicken Burger',
     desc: 'A Double Patty A Double Chicken Burger with cheese, tomato, cucumber, lettuce and caramelized onions, Topped with lettuce, tomatoes, cheese and spicy mayo, they are the most delicious, easy dinner and a guaranteed crowd pleaser. Unlike ground chicken burgers that require chicken patties made with ground chicken (or ground turkey), bread crumbs, etc. these juicy chicken burgers need only a handful of ingredients and can be made in a fraction of the time. The chicken is marinated in an aromatic mixture of olive oil, garlic, lemon and spices.',
-    image: '/img/burger-18.jpg',
+    image: '/img/burger-24.jpg',
     ingredients: [
       'cheese',
       'tomato',
