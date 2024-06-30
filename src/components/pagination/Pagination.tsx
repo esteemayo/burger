a0b54e6faca9c0c708a7af2af898b008eb1f93f4
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-30 11:50:35 +0100

    feat: add PaginationProps static prop checking annotation

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,6 +1,8 @@
+import { PaginationProps } from '@/types';
+
 import './Pagination.scss';
 
-const Pagination = () => {
+const Pagination = ({ disabled }: PaginationProps) => {
   return <div className='pagination'>
     <button type='button'>1</button>
     <button type='button'>2</button>
