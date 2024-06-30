c9117b53d104b8fad53c97ef39dc37fac9523c1f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-30 11:46:33 +0100

    feat: add html markup

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,7 +1,11 @@
 import './Pagination.scss';
 
 const Pagination = () => {
-  return <div>Pagination</div>;
+  return <div className='pagination'>
+    <button type='button'>1</button>
+    <button type='button'>2</button>
+    <button type='button'>3</button>
+  </div>;
 };
 
 export default Pagination;
