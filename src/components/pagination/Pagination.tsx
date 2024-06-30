38592a88ba14113421827b59204f28552d0f74ab
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-30 11:58:39 +0100

    fix: modify

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -5,7 +5,7 @@ import './Pagination.scss';
 const Pagination = ({ disabled }: PaginationProps) => {
   return (
     <div className='pagination'>
-      <div className='container'>
+      <div className='wrapper'>
         <button type='button' disabled={disabled}>1</button>
         <button type='button' disabled={disabled}>2</button>
         <button type='button' disabled={disabled}>3</button>
