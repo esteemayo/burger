683603b8e1f1d1202ee06f4cd3fd72a1f3361ce0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-30 11:54:26 +0100

    feat: add html markup

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -3,11 +3,15 @@ import { PaginationProps } from '@/types';
 import './Pagination.scss';
 
 const Pagination = ({ disabled }: PaginationProps) => {
-  return <div className='pagination'>
-    <button type='button' disabled={disabled}>1</button>
-    <button type='button' disabled={disabled}>2</button>
-    <button type='button' disabled={disabled}>3</button>
-  </div>;
+  return (
+    <div className='pagination'>
+      <div className='container'>
+        <button type='button' disabled={disabled}>1</button>
+        <button type='button' disabled={disabled}>2</button>
+        <button type='button' disabled={disabled}>3</button>
+      </div>
+    </div>
+  );
 };
 
 export default Pagination;
