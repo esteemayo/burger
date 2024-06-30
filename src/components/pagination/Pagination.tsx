21cc2e607d9eef861b8a18130430ada83710715f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-06-30 20:12:00 +0100

    feat: style pagination button

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -6,9 +6,19 @@ const Pagination = ({ disabled }: PaginationProps) => {
   return (
     <div className='pagination'>
       <div className='wrapper'>
-        <button type='button' disabled={disabled}>1</button>
-        <button type='button' disabled={disabled}>2</button>
-        <button type='button' disabled={disabled}>3</button>
+        <button
+          type='button'
+          disabled={disabled}
+          className='btnPagination active'
+        >
+          1
+        </button>
+        <button type='button' disabled={disabled} className='btnPagination'>
+          2
+        </button>
+        <button type='button' disabled={disabled} className='btnPagination'>
+          3
+        </button>
       </div>
     </div>
   );
