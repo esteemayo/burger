9d1d8550cae1dcd6e4ffdba2f4c0bcbe0cea282d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-02 19:53:46 +0100

    fix: modify

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -9,8 +9,8 @@ export const getProducts = () => http.get('/api/products');
 export const getFeaturedProducts = () =>
   http.get('/api/products?featured=true');
 
-export const searchProducts = (searchQuery: string) =>
-  http.get(`/api/products/search?q=${searchQuery}`);
+export const searchProducts = (searchQuery: string, page: number) =>
+  http.get(`/api/products/search?q=${searchQuery}&page=${page}`);
 
 export const getProduct = (productId: string) =>
   http.get(productUrl(productId));
