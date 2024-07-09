26451182836a8ff93aefc8c098dc9c71cd7a673b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-10 00:37:25 +0100

    feat: add bars icon

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -115,6 +115,22 @@ const Navbar = () => {
               </div>
             )}
         </div>
+        <div className='hamburger'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='size-6'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+            />
+          </svg>
+        </div>
       </div>
     </nav>
   );
