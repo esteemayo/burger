6f17a44615f669d72cf4fd4a2569b56cccc9423d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-09 01:00:17 +0100

    feat: add html markup

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,52 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import RoomServiceIcon from '@mui/icons-material/RoomService';
+
+import { userLinks } from '@/data';
+
 import './Sidebar.scss';
 
 const Sidebar = () => {
   return (
     <aside className='sidebar'>
-      <div className='container'>Sidebar</div>
+      <div className='container'>
+        <ul className='lists'>
+          <li>
+            <Link href='/products'>
+              <RoomServiceIcon />
+              <span>Products</span>
+            </Link>
+          </li>
+          {userLinks.map((link) => {
+            const { id, url, icon, label } = link;
+            return (
+              <li key={id}>
+                <Link href={url}>
+                  <Image
+                    src={`/svg/${icon}.svg`}
+                    width={15}
+                    height={15}
+                    alt=''
+                  />
+                  <span className='userLabel'>{label}</span>
+                </Link>
+              </li>
+            );
+          })}
+          <li>
+            <Image
+              src='/svg/shopping-cart.svg'
+              width={15}
+              height={15}
+              alt='search icon'
+              className='shoppingCartLogo'
+            />
+            <span className='count'>
+              <Link href='/checkout'>Cart ({0})</Link>
+            </span>
+          </li>
+        </ul>
+      </div>
     </aside>
   );
 };
