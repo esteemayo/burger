b9c45df9693b632c6a1c208ff777a5673c3183c3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-10 18:06:22 +0100

    feat: add styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,6 +11,7 @@ import CartMenu from '../cartMenu/CartMenu';
 import Logo from '../logo/Logo';
 import UserMenu from '../userMenu/UserMenu';
 import Search from '../search/Search';
+import ToggleButton from '../toggleButton/ToggleButton';
 
 import { useCartStore } from '@/hooks/useCartStore';
 import { useSearch } from '@/hooks/useSearch';
@@ -117,22 +118,7 @@ const Navbar = () => {
               </div>
             )}
         </div>
-        <div className='hamburger' onClick={onOpen}>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='size-6'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
-            />
-          </svg>
-        </div>
+        <ToggleButton onOpen={onOpen} />
       </div>
     </nav>
   );
