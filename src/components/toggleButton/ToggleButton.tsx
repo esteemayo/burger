c80cf1b85e80f849eb8cfb6b50c325153ab8b5f6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-10 18:00:07 +0100

    feat: create ToggleButton component

---
+++ src/components/toggleButton/ToggleButton.tsx
@@ -0,0 +1,28 @@
+import './ToggleButton.scss';
+
+interface ToggleButtonProps {
+  onOpen(): void;
+}
+
+const ToggleButton = ({ onOpen }: ToggleButtonProps) => {
+  return (
+    <button type='button' className='toggleButton' onClick={onOpen}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={1.5}
+        stroke='currentColor'
+        className='size-6'
+      >
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default ToggleButton;
