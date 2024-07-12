51eb07860d530491a11d1752246333887f31b8ce
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-12 01:40:22 +0100

    feat: dynamically fetched related products

--- src/components/relatedProducts/RelatedProducts.tsx
+++ src/components/relatedProducts/RelatedProducts.tsx
@@ -1,26 +1,28 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 import RelatedProduct from '../relatedProduct/RelatedProduct';
 import RelatedCardSkeleton from '../relatedCardSkeleton/RelatedCardSkeleton';
 
-import { relatedProducts } from '@/data';
+import { getRelatedProducts } from '@/services/productService';
 
 import './RelatedProducts.scss';
 
 interface RelatedProductsProps {
   productId: string;
+  ingredients?: string[];
 }
 
-const RelatedProducts = ({ productId }: RelatedProductsProps) => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 5000);
-  }, []);
+const RelatedProducts = ({ productId, ingredients }: RelatedProductsProps) => {
+  const { isLoading, data: products }  = useQuery({
+    queryKey: ['featuredProducts'],
+    queryFn: async () => {
+      const { data } = await getRelatedProducts(ingredients);
+      return data;
+    },
+    enabled: !!ingredients,
+  });
 
   return (
     <section className='relatedProducts'>
@@ -28,10 +30,13 @@ const RelatedProducts = ({ productId }: RelatedProductsProps) => {
         <h2 className='relatedHeader'>Related products</h2>
         <div className='relatedWrap'>
           {isLoading
-            ? relatedProducts.map((item) => {
+            ? products?.map((item) => {
                 return <RelatedCardSkeleton key={item.id} />;
               })
-            : relatedProducts.map((product) => {
+            : products
+              ?.filter((product) => product.id !== productId)
+              .slice(0, 4)
+              .map((product) => {
                 return <RelatedProduct key={product.id} product={product} />;
               })}
         </div>
