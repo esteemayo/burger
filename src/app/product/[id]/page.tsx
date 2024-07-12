4273029ff283b6e686e8846e26171761875d92c1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-12 12:03:36 +0100

    fix: refactor

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,20 +1,7 @@
 import type { Metadata } from 'next';
 
 import ProductClient from './ProductClient';
-import EmptyState from '@/components/emptyState/EmptyState';
-
-import { SingleProductType } from '@/types';
 import { getProduct } from '@/services/productService';
-
-const getData = async (productId: string) => {
-  const { data, status } = await getProduct(productId);
-
-  if (status !== 200) {
-    throw new Error('Failed!');
-  }
-
-  return data;
-};
 
 interface IParams {
   params: {
@@ -33,18 +20,6 @@ export async function generateMetadata({ params }: IParams): Promise<Metadata> {
 }
 
 const Product = async ({ params: { id } }: IParams) => {
-  const product: SingleProductType = await getData(id);
-
-  // if (!product) {
-  //   return (
-  //     <EmptyState
-  //       title='Product not found'
-  //       subtitle="Looks like there's no product with that ID."
-  //       imgSrc='empty'
-  //     />
-  //   );
-  // }
-
   return <ProductClient productId={id} />;
 };
 
