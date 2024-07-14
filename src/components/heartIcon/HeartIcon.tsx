debbe76d4b77e86f33707cfa3a893e58db7f2d12
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-14 02:44:27 +0100

    fix: rename svg files

--- src/components/heartIcon/HeartIcon.tsx
+++ src/components/heartIcon/HeartIcon.tsx
@@ -6,9 +6,9 @@ interface HeartIconProps {
 
 const HeartIcon = ({ isFavorite }: HeartIconProps) => {
   return isFavorite ? (
-    <Image src='/svg/heart.svg' width={20} height={20} alt='heart icon' />
-  ) : (
     <Image src='/svg/heart-1.svg' width={20} height={20} alt='heart icon' />
+  ) : (
+    <Image src='/svg/heart-2.svg' width={20} height={20} alt='heart icon' />
   );
 };
 
