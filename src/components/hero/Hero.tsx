b76a0a94e164aaedabe90086717a612d562620ba
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-14 16:47:55 +0100

    feat: add styling

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,10 +1,18 @@
+'use client';
+
+import { useMemo } from 'react';
+
 import { HeroProps } from '@/types';
 
 import './Hero.scss';
 
 const Hero = ({ name, image }: HeroProps) => {
+  const headerBcg = useMemo(() => {
+    return `linear-gradient(to bottom right, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75)),url(${image})`;
+  }, [image]);
+
   return (
-    <header className='hero' style={{ backgroundImage: `url(${image})` }}>
+    <header className='hero' style={{ backgroundImage: headerBcg }}>
       <div className='headingWrap'>
         <h1 className='heroHeading'>{name}</h1>
         <div className='heroSubHeading'>
