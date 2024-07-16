b1431abfdf5ac249d5c0f3da291db44cc2f66901
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-16 03:08:44 +0100

    feat: fetch reviewer details

--- src/components/review/Review.tsx
+++ src/components/review/Review.tsx
@@ -1,18 +1,32 @@
+'use client';
+
 import Image from 'next/image';
+import { useQuery } from '@tanstack/react-query';
 
 import { ReviewProps } from '@/types';
 import StarRating from '../starRating/StarRating';
 
+import { getUser } from '@/services/userService';
+
 import './Review.scss';
 
 const Review = ({ desc, rating, userId }: ReviewProps) => {
+  const { data: user } = useQuery({
+    queryKey: [`${userId}`],
+    queryFn: async () => {
+      const { data } = await getUser(userId);
+      return data;
+    },
+    enabled: !!userId,
+  });
+
   return (
     <article className='review'>
       <div className='reviewCard'>
         <div className='reviewWrapper'>
           <div className='reviewImg'>
             <Image
-              src={'/svg/male-avatar.svg'}
+              src={user?.image ?? '/svg/male-avatar.svg'}
               width={87}
               height={87}
               alt='avatar'
@@ -25,7 +39,7 @@ const Review = ({ desc, rating, userId }: ReviewProps) => {
           </div>
           <div className='reviewer'>
             <div className='reviewerWrap'>
-              <div className='reviewerName'>{userId}</div>
+              <div className='reviewerName'>{user?.name}</div>
               <p className='reviewerText'>{desc}</p>
             </div>
           </div>
