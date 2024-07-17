7d40337ccc37ad2a39e2939d531f24fd8c144356
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-17 16:50:33 +0100

    fix: modify

--- src/validations/review.ts
+++ src/validations/review.ts
@@ -4,19 +4,11 @@ export const validateReviewInputs = (
   data: ReviewData,
   rating: null | number
 ) => {
-  const { desc, name, email } = data;
+  const { desc} = data;
   const errors: ReviewErrors = {};
 
   if (desc.trim() === '') {
     errors.desc = 'A review must have a description';
-  }
-
-  if (name.trim() === '') {
-    errors.name = "A review must have a user's name";
-  }
-
-  if (email.trim() === '') {
-    errors.email = "A review must have a user's email address";
   }
 
   if (!rating) {
