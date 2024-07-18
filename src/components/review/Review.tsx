0f61cfd46b9f015dd4bac9bcda57acf4d1877037
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-19 00:41:29 +0100

    fix: modify

--- src/components/review/Review.tsx
+++ src/components/review/Review.tsx
@@ -39,23 +39,25 @@ const Review = ({ desc, rating, userId }: ReviewProps) => {
     <article className='review'>
       <div className='reviewCard'>
         <div className='reviewWrapper'>
-          <div className='reviewImg'>
-            <Image
-              src={reviewerImage}
-              width={87}
-              height={87}
-              alt='avatar'
-            />
+          <div className='reviewBox'>
+            <div className='reviewImg'>
+              <Image
+                src={reviewerImage}
+                width={87}
+                height={87}
+                alt='avatar'
+              />
+            </div>
+            <div className='reviewer'>
+              <div className='reviewerWrap'>
+                <div className='reviewerName'>{user?.name}</div>
+                <p className='reviewerText'>{desc}</p>
+              </div>
+            </div>
           </div>
           <div className='reviewRating'>
             <div className='star'>
               <StarRating name='read-only' value={rating} readOnly />
-            </div>
-          </div>
-          <div className='reviewer'>
-            <div className='reviewerWrap'>
-              <div className='reviewerName'>{user?.name}</div>
-              <p className='reviewerText'>{desc}</p>
             </div>
           </div>
         </div>
