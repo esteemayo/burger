057bfa089f9a36455cf70c2bb16aa7ecb8ffee43
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-19 02:35:08 +0100

    fix: modify

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -5,7 +5,7 @@ import SuccessClient from './SuccessClient';
 import './Success.scss';
 
 export const metadata: Metadata = {
-  title: 'Burger - Success page',
+  title: 'Burger | Success',
 };
 
 const Success = () => {
