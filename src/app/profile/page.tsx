0cb7ca3d78b0389498447f47d0e4ff25779ee6e1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-19 01:48:15 +0100

    fix: modify

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,7 +6,7 @@ import AccountSettings from '@/components/accountSettings/AccountSettings';
 import './Profile.scss';
 
 export const metadata: Metadata = {
-  title: 'Burger - User profile page',
+  title: 'Burger | User Profile',
 };
 
 const Profile = () => {
