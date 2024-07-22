82105d2817381dc1fdb0ca8cbe7cf4f78cf40930
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-22 01:49:26 +0100

    fix: refactor

--- src/components/userMenu/UserMenu.tsx
+++ src/components/userMenu/UserMenu.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link';
 import { signOut } from 'next-auth/react';
 import Image from 'next/image';
 
-import { authLinks } from '@/data';
 import { UserMenuProps } from '@/types';
+import { authLinks, userLinks } from '@/data';
 
 import './UserMenu.scss';
 
@@ -42,40 +42,28 @@ const UserMenu = ({ currentUser }: UserMenuProps) => {
       ) : (
         <div className='userController'>
           <div className='userLinks'>
-            <Link href='/orders' className='userLink'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='size-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'
-                />
-              </svg>
-              <span className='userLabel'>Upcoming orders</span>
-            </Link>
-            <Link href='/profile' className='userLink'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='size-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z'
-                />
-              </svg>
-              <span className='userLabel'>Account</span>
-            </Link>
+            {userLinks.map((link) => {
+              const { id, url, icon, label } = link;
+              return (
+                <Link key={id} href={url} className='userLink'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='size-6'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d={icon}
+                    />
+                  </svg>
+                  <span className='userLabel'>{label}</span>
+                </Link>
+              );
+            })}
           </div>
           <div className='userFooter'>
             <span>{currentUser.name}</span>
