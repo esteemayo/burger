27fb62509dea890bd5e55fd4b66421eee4c56a5c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-22 19:22:41 +0100

    fix: modify

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 import SuccessClient from './SuccessClient';
 
@@ -9,6 +10,13 @@ export const metadata: Metadata = {
 };
 
 const Success = () => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const payment_intent = params.get('payment_intent');
+
+  console.log(payment_intent);
+
   return (
     <div className='success'>
       <div className='container'>
