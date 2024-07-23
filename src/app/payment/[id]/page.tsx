df101d2ab72f5c82ce3bea03fd61676649c90d35
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-23 01:46:32 +0100

    fix: modify

--- src/app/payment/[id]/page.tsx
+++ src/app/payment/[id]/page.tsx
@@ -37,7 +37,7 @@ const Payment = ({ params }: IParams) => {
         const { data } = await makePayment(orderId);
 
         setClientSecret(data?.clientSecret);
-        // reset();
+        reset();
       } catch (err: unknown) {
         console.log(err);
       }
