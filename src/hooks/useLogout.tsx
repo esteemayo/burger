705de5a7ce9164e1e893c7f74e1e629e37627f13
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-23 02:07:26 +0100

    feat: add useLogout custom hook

---
+++ src/hooks/useLogout.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useCallback } from 'react';
+import { signOut } from 'next-auth/react';
+
+import { useSidebar } from './useSidebar';
+
+export const useLogout = () => {
+  const isOpen = useSidebar((store) => store.isOpen);
+  const onClose = useSidebar((store) => store.onClose);
+
+  const handleClose = useCallback(() => {
+    if (isOpen) {
+      onClose();
+    }
+  }, [isOpen, onClose]);
+
+  const handleLogout = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      signOut({
+        callbackUrl: '/',
+      });
+
+      handleClose?.();
+    },
+    [handleClose]
+  );
+
+  return {
+    handleLogout,
+  };
+};
