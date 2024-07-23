3ddf4aff1f7b6b8d6e6416d277ac71dfa097c344
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-23 01:23:22 +0100

    feat: add update order payment status api endpoint

--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -2,3 +2,6 @@ import http from './httpService';
 
 export const makePayment = (orderId: string) =>
   http.post(`/api/create-intent/${orderId}`);
+
+export const updatePaymentIntent = (paymentIntent: string | null) =>
+  http.patch(`/confirm/${paymentIntent}`);
