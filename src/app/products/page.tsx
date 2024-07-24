6ba769859b7394bd165b28a0a37512d61fe1d05c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-24 01:52:46 +0100

    fix: modify

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import ProductsClient from './ProductsClient';
 
 export const metadata: Metadata = {
-  title: 'Burger | Products',
+  title: 'Burger. | Products',
 };
 
 const Products = () => {
