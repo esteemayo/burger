45df7522d7d6ece1fb12740302ce7597a07c824d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-24 01:24:31 +0100

    feat: create PaymentForm compoennt

---
+++ src/components/paymentForm/PaymentForm.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { Elements } from '@stripe/react-stripe-js';
+import { useEffect, useState } from 'react';
+import {
+  Appearance,
+  StripeElementsOptions,
+  loadStripe,
+} from '@stripe/stripe-js';
+
+import { useCartStore } from '@/hooks/useCartStore';
+import { makePayment } from '@/services/paymentService';
+
+import CheckoutForm from '@/components/checkoutForm/CheckoutForm';
+
+import './Payment.scss';
+
+interface PaymentFormProps {
+  orderId: string;
+}
+
+const stripePromise = loadStripe(
+  'pk_test_51PMC1YLjKbhA4Yjg89VxYW1rFYaqbOZ3TQqbc4AtBY0PMnQzwJAad7eOBO1HWFbSgrHY1E0MpqQvx94jrZhvx2Bq00jwNTSLcT'
+);
+
+const PaymentForm = ({ orderId }: PaymentFormProps) => {
+  const reset = useCartStore((store) => store.reset);
+
+  const [clientSecret, setClientSecret] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await makePayment(orderId);
+
+        setClientSecret(data?.clientSecret);
+        reset();
+      } catch (err: unknown) {
+        console.log(err);
+      }
+    })();
+  }, [orderId, reset]);
+
+  const appearance: Appearance = {
+    theme: 'stripe',
+  };
+
+  const options: StripeElementsOptions = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className='payment'>
+      <div className='container'>
+        {clientSecret && (
+          <Elements options={options} stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PaymentForm;
