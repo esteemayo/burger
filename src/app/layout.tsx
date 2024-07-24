c8770cb9ec53e157defbce890629368a56bc7ab7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-24 01:55:05 +0100

    fix: modify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: 'Burger',
+  title: 'Burger.',
   description: 'Juicy Burger',
   authors: [
     { name: 'Esteem Designs', url: 'https://www.esteemdesigns.com' },
