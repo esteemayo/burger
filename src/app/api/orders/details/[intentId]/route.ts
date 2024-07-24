7f10c6672f3e1113387fb65849155099293fb885
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-24 01:09:39 +0100

    fix: add user authentication

--- src/app/api/orders/details/[intentId]/route.ts
+++ src/app/api/orders/details/[intentId]/route.ts
@@ -11,28 +11,35 @@ interface IParams {
 
 export const GET = async (req: NextRequest, { params }: IParams) => {
   const { intentId } = params;
+  const session = await getAuthSession();
 
-  try {
-    const order = await prisma.order.findUnique({
-      where: {
-        intent_id: intentId,
-      },
-    });
+  if (session) {
+    try {
+      const order = await prisma.order.findUnique({
+        where: {
+          intent_id: intentId,
+        },
+      });
 
-    if (!order) {
+      if (!order) {
+        return new NextResponse(
+          JSON.stringify({
+            message: `No order found with the given IntentID → ${intentId}`,
+          }),
+          { status: 404 }
+        );
+      }
+
+      return new NextResponse(JSON.stringify(order), { status: 200 });
+    } catch (err: unknown) {
       return new NextResponse(
-        JSON.stringify({
-          message: `No order found with the given IntentID → ${intentId}`,
-        }),
-        { status: 404 }
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
       );
     }
-
-    return new NextResponse(JSON.stringify(order), { status: 200 });
-  } catch (err: unknown) {
-    return new NextResponse(
-      JSON.stringify({ message: 'Something went wrong' }),
-      { status: 500 }
-    );
   }
+  return new NextResponse(
+    JSON.stringify({ message: 'You are not authenticated' }),
+    { status: 401 }
+  );
 };
