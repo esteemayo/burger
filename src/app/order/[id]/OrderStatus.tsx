cf8e19b607ca5b191ed2d681dc9a4c0ad6193ac6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-07-30 18:49:30 +0100

    fix: modify

--- src/app/order/[id]/OrderStatus.tsx
+++ src/app/order/[id]/OrderStatus.tsx
@@ -20,7 +20,13 @@ const OrderStatus = ({ status }: OrderStatusProps) => {
     return statusLists.findIndex((item) => item === status);
   }, [status]);
 
-  console.log(statusIndex)
+  console.log(statusIndex);
+  console.log(statusLists[statusIndex]);
+
+  const statusClass = (index: number) => {
+    if (index - statusIndex < 1) return 'done';
+    if (index - statusIndex > 1) return 'ready';
+  };
 
   return (
     <div className='orderStatus'>
@@ -31,19 +37,19 @@ const OrderStatus = ({ status }: OrderStatusProps) => {
         </p>
       </div>
       <ul className='stepper'>
-        <li className={status !== 'not paid' ? 'done' : 'ready'}>
+        <li className={statusClass(0)}>
           <div className='item'>Order confirmed</div>
         </li>
-        <li className={status !== 'preparing' ? 'done' : 'ready'}>
+        <li className={statusClass(1)}>
           <div className='item'>Start production</div>
         </li>
         {/* <li className='ready'>
           <div className='item'>Quality check</div>
         </li> */}
-        <li className={status !== 'on the way' ? 'done' : 'ready'}>
+        <li className={statusClass(2)}>
           <div className='item'>Dispatched item</div>
         </li>
-        <li className={status !== 'delivered' ? 'done' : 'ready'}>
+        <li className={statusClass(3)}>
           <div className='item'>Product delivered</div>
         </li>
       </ul>
