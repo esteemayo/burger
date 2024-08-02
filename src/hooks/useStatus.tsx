70d1a2ef741d55cf4dbe9d017c95b10317999958
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-02 07:32:50 +0100

    fix: modify

--- src/hooks/useStatus.tsx
+++ src/hooks/useStatus.tsx
@@ -17,7 +17,7 @@ export const useStatus = (status: StatusType) => {
   const statusClass = useCallback(
     (index: number) => {
       if (index - statusIndex < 1) return 'done';
-      if (index - statusIndex === 1) return 'ready';
+      if (index - statusIndex === 1) return 'inProgress';
       if (index - statusIndex > 1) return 'undone';
     },
     [statusIndex]
