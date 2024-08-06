1c2f1633786a21af58e3c79e5c07326bf1151d14
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-06 03:23:38 +0100

    feat: add handleClick function

--- src/components/newsletter/NewsLetter.tsx
+++ src/components/newsletter/NewsLetter.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import { ClipLoader } from 'react-spinners';
@@ -8,7 +8,14 @@ import { ClipLoader } from 'react-spinners';
 import './NewsLetter.scss';
 
 const NewsLetter = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = useCallback((_e: React.MouseEvent<HTMLDivElement>) => {
+    if (inputRef) {
+      inputRef.current?.focus();
+    }
+  }, []);
 
   const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -33,14 +40,18 @@ const NewsLetter = () => {
     <section className='newsLetter'>
       <div className='container'>
         <div className='textWrap'>
-          <div className='headingWrap'>
+          <div className='headingWrap' onClick={handleClick}>
             <EmailOutlinedIcon />
             <h5>Subscribe to special offers</h5>
           </div>
         </div>
         <div className='formWrap'>
           <form onSubmit={handleSubmit}>
-            <input type='email' placeholder='Your email address' />
+            <input
+              ref={inputRef}
+              type='email'
+              placeholder='Your email address'
+            />
             <button type='submit' disabled={isLoading}>
               {isLoading ? (
                 <ClipLoader size={20} color='#a00c1a' />
