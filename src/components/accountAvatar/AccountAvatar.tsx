abaa58ac5c0536cc2c487b7e59c7fe00feea70e9
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-08 05:17:41 +0100

    feat: create AccountAvatar component

---
+++ src/components/accountAvatar/AccountAvatar.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Image from 'next/image';
+
+const AccountAvatar = ({ file, avatar, onChange }) => {
+  return (
+    <div className='accountAvatar'>
+      <div className='dataImg'>
+        <Image
+          src={file ? URL.createObjectURL(file as Blob | MediaSource) : avatar}
+          width={50}
+          height={50}
+          alt='avatar'
+          className='userAvatar'
+        />
+        <label htmlFor='file'>
+          <Image
+            src='/svg/pencil.svg'
+            width={17}
+            height={17}
+            alt='pencil icon'
+            className='icon'
+          />
+        </label>
+        <input id='file' name='file' type='file' onChange={onChange} />
+      </div>
+    </div>
+  );
+};
+
+export default AccountAvatar;
