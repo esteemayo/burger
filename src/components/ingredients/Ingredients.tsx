565bb6107dde0fd58732b549c3d2940860f7bff6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-09 01:49:37 +0100

    feat: create Ingredients component

---
+++ src/components/ingredients/Ingredients.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Image from 'next/image';
+import { ClipLoader } from 'react-spinners';
+
+import './Ingredients.scss';
+
+const Ingredients = ({ ingredients, loading, disabled, onAdd, onDelete }) => {
+  return (
+    <div className='ingredients'>
+      <div className='wrapper'>
+        {ingredients?.map((item) => {
+          return (
+            <span key={item}>
+              {item}
+              <Image
+                src='/svg/x-mark.svg'
+                width={15}
+                height={15}
+                alt='delete icon'
+                onClick={(e) => onDelete(e, item)}
+              />
+            </span>
+          );
+        })}
+      </div>
+      <button type='button' disabled={!!disabled || loading} onClick={onAdd}>
+        {loading ? <ClipLoader size={13} color='#a00c1a' /> : 'Add'}
+      </button>
+    </div>
+  );
+};
+
+export default Ingredients;
