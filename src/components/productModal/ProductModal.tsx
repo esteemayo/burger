d579a1a313ca047802b8af19d5fa1630a13b3f1f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-09 01:41:44 +0100

    fix: modify

--- src/components/productModal/ProductModal.tsx
+++ src/components/productModal/ProductModal.tsx
@@ -37,11 +37,11 @@ const ProductModal = () => {
   const [file, setFile] = useState<File[]>();
   const [ingredient, setIngredient] = useState('');
   const [step, setStep] = useState(STEPS.INFO);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState<ProductErrors>({});
   const [data, setData] = useState(initialState);
   const [ingredients, setIngredients] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [loading, setLoading] = useState(false);
 
   const onPrev = useCallback(() => {
     setStep((value) => {
