92d104001f89e3f1e0a155c7be223bb393075726
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-09 01:56:37 +0100

    fix: refactor

--- src/components/ingredientInput/IngredientInput.tsx
+++ src/components/ingredientInput/IngredientInput.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import { useMemo } from 'react';
-import Image from 'next/image';
-import { ClipLoader } from 'react-spinners';
 
+import Ingredients from '../ingredients/Ingredients';
 import Input from '../input/Input';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
@@ -34,31 +33,13 @@ const IngredientInput = ({
         onChange={onChange}
       />
       {error && <ErrorMessage message={error} />}
-      <div className='ingredientLists'>
-        <div className='ingredients'>
-          {ingredients?.map((item) => {
-            return (
-              <span key={item}>
-                {item}
-                <Image
-                  src='/svg/x-mark.svg'
-                  width={15}
-                  height={15}
-                  alt='delete icon'
-                  onClick={(e) => onDelete(e, item)}
-                />
-              </span>
-            );
-          })}
-        </div>
-        <button
-          type='button'
-          disabled={!!disableBtn || loading}
-          onClick={onAdd}
-        >
-          {loading ? <ClipLoader size={13} color='#a00c1a' /> : 'Add'}
-        </button>
-      </div>
+      <Ingredients
+        ingredients={ingredients}
+        loading={loading}
+        disabled={disableBtn}
+        onAdd={onAdd}
+        onDelete={onDelete}
+      />
     </div>
   );
 };
