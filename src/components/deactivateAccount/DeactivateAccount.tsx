d56dfe3f5559d6c8dccfc10b49f69cfcecd36e49
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-10 02:40:02 +0100

    fix: refactor

--- src/components/deactivateAccount/DeactivateAccount.tsx
+++ src/components/deactivateAccount/DeactivateAccount.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import { useCallback } from 'react';
 
 import { useAccountModal } from '@/hooks/useAccountModal';
 
@@ -11,7 +14,23 @@ interface DeactivateAccountProps {
 }
 
 const DeactivateAccount = ({ isAdmin }: DeactivateAccountProps) => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const onOpen = useAccountModal((state) => state.onOpen);
+
+  const handleOpen = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      if (session) {
+        onOpen();
+        return;
+      }
+      return router.push('/login');
+    },
+    [onOpen, router, session]
+  );
 
   if (isAdmin) {
     return;
@@ -19,7 +38,7 @@ const DeactivateAccount = ({ isAdmin }: DeactivateAccountProps) => {
 
   return (
     <div className='deactivateAccount'>
-      <button type='button' onClick={onOpen}>
+      <button type='button' onClick={handleOpen}>
         <Image src='/svg/user-minus.svg' width={15} height={15} alt='icon' />
         Delete my Account
       </button>
