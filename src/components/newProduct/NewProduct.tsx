5456136397f904c47544e0ff296241bff489077c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-10 02:30:15 +0100

    fix: refactor

--- src/components/newProduct/NewProduct.tsx
+++ src/components/newProduct/NewProduct.tsx
@@ -1,17 +1,33 @@
 'use client';
 
 import Image from 'next/image';
+import { useCallback } from 'react';
 
 import { useProductModal } from '@/hooks/useProductModal';
 
 import './NewProduct.scss';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
-interface NewProductProps { 
+interface NewProductProps {
   isAdmin: Boolean | undefined;
 }
 
 const NewProduct = ({ isAdmin }: NewProductProps) => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const onOpen = useProductModal((state) => state.onOpen);
+
+  const handleOpen = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+
+    if (session) {
+      onOpen();
+      return;
+    }
+    return router.push('/login');
+  }, []);
 
   if (!isAdmin) {
     return;
@@ -19,7 +35,7 @@ const NewProduct = ({ isAdmin }: NewProductProps) => {
 
   return (
     <div className='newProduct'>
-      <button type='button' onClick={onOpen}>
+      <button type='button' onClick={handleOpen}>
         <Image src='/svg/plus.svg' width={15} height={15} alt='plus icon' />
         Add new product
       </button>
