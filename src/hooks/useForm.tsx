cb2c3cef0ef71948050552ec5b03a6f1784f4773
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-11 18:48:15 +0100

    fix: clear error validation

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const useForm = <T extends object, U extends object>(
   callback: () => void,
@@ -42,6 +42,12 @@ export const useForm = <T extends object, U extends object>(
     },
     [callback, data, initialErrors, initialState, validate]
   );
+
+  useEffect(() => {
+    if (Object.keys(errors).length > 0) {
+      setTimeout(() => setErrors(initialStateErrors), 5000);
+    }
+  }, [errors]);
 
   return {
     data,
