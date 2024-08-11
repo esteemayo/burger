299a2fd07595bbca53e55dad5601a63682d626fe
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-11 08:02:06 +0100

    fix: refactor

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -1,7 +1,9 @@
 import { ClipLoader } from 'react-spinners';
 
-const Spinner = () => {
-  return <ClipLoader size={20} color='#fff' />;
+import { SpinnerProps } from '@/types';
+
+const Spinner = ({ size = 20, color = '#fff ' }: SpinnerProps) => {
+  return <ClipLoader size={size} color={color} />;
 };
 
 export default Spinner;
