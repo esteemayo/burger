a67242681046f1ed948d8203314e3f47ac726790
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-12 19:13:23 +0100

    fix: modify

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -64,10 +64,6 @@ const Sidebar = () => {
     return window.removeEventListener('keydown', handleEscape);
   }, [handleEscape]);
 
-  if (!isOpen) {
-    return;
-  }
-
   return (
     <aside className={sidebarClasses} onClick={closeHandler}>
       <div className={containerClasses}>
