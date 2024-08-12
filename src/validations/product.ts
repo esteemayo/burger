d259ee9d4b8d633b40b711e45574d73cad132ec3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-12 19:45:27 +0100

    fix: modify

--- src/validations/product.ts
+++ src/validations/product.ts
@@ -17,7 +17,7 @@ export const validateProductInput = (
 
   if (!price) {
     errors.price = 'Price is required';
-  } else if (price < 1) {
+  } else if (price.startsWith('0')) {
     errors.price = 'Price must be equal or greater than 1';
   }
 
