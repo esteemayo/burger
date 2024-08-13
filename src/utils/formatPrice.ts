dcd293cc0870783de5a06c373d0e3a24cd8f2818
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-13 01:56:05 +0100

    fix: modify

--- src/utils/formatPrice.ts
+++ src/utils/formatPrice.ts
@@ -1,12 +1,6 @@
 export const formatPrice = (price: string) => {
-  const cleanedValue = price
-    .replace(/[^0-9.]/g, '')
-    .replace(/(\..*)\./g, '$1');
-
-  const formattedPrice = `${cleanedValue.replace(
-    /(\d)(?=(\d{3})+(?!\d))/g,
-    '$1,'
-  )}`;
+  const cleanedValue = price.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+  const formattedPrice = cleanedValue.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 
   return {
     formattedPrice,
