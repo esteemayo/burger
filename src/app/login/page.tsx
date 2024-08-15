96feef6343d0553cf61ad44d08c151658de1bfc5
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-15 02:19:04 +0100

    fix: modify

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,8 @@ import LoginClient from './LoginClient';
 
 export const metadata: Metadata = {
   title: 'Burger. | Login',
-  description: 'Log into your account',
+  description:
+    'Access your food account and unlock a world of flavors! Log in to your Burger. account to discover new recipes, order food online, and manage your meal planning. Sign in now and start cooking a storm',
   keywords:
     'Burger app login, food delivery sign in, online food ordering login, food ordering and delivery login',
 };
