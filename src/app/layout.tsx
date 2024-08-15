bcfc1da5f1bf272b5435553b10931e1328f65b63
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-15 19:16:09 +0100

    fix: modify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,8 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: 'Burger.',
-  description: 'Juicy Burger',
+  description: 
+    'Order food online from local restaurants with Burger. Get delivery or pickup from your favorite eateries.',
   keywords: 'food ordering, online food ordering',
   authors: [
     { name: 'Xsteem Designx', url: 'https://www.esteemdesigns.com' },
