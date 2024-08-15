e6f99fe6884cc4ea950840c107970ef527201a86
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-15 02:23:24 +0100

    fix: modify

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,7 +4,8 @@ import RegisterClient from './RegisterClient';
 
 export const metadata: Metadata = {
   title: 'Burger. App Register | Create Your Account',
-  description: 'Create your account',
+  description:
+    'Create your food account and unlock a world of flavors! Sign up for your Burger. account to discover new recipes, order food online, and manage your meal planning. Sign up now and start cooking a storm',
   keywords:
     'Burger app signup, food delivery sign up, online food ordering signup, food ordering and delivery signup',
 };
