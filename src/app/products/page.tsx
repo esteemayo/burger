34c7de13730fed12c2a8458f648a8f8e0d20bf5e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-15 19:28:12 +0100

    fix: handle SEO

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import ProductsClient from './ProductsClient';
 
 export const metadata: Metadata = {
-  title: 'Burger. | Products',
+  title: 'Burger. App | Products',
 };
 
 const Products = () => {
