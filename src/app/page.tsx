ecb3cc54b3bc439ea75f6124a7a4beca4e48b106
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-15 19:32:09 +0100

    fix: modify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import type { Metadata } from 'next';
-
 import Header from '@/components/header/Header';
 import Menus from '@/components/menus/Menus';
 import Features from '@/components/features/Features';
 import Offer from '@/components/offer/Offer';
 import NewsLetter from '@/components/newsletter/NewsLetter';
-
-export const metadata: Metadata = {
-  title: 'Burger. App | Home',
-};
 
 const Home = () => {
   return (
