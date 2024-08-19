9b4ec4b10a64dafdb5621a05d045f3ac8dd74d98
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-19 01:24:31 +0100

    fix: refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-import Header from '@/components/header/Header';
-import Menus from '@/components/menus/Menus';
-import Features from '@/components/features/Features';
-import Offer from '@/components/offer/Offer';
-import NewsLetter from '@/components/newsletter/NewsLetter';
+import HomeClient from './HomeClient';
 
 const Home = () => {
-  return (
-    <>
-      <Header />
-      <Features />
-      <Menus />
-      <Offer />
-      <NewsLetter />
-    </>
-  );
+  return <HomeClient />;
 };
 
 export default Home;
