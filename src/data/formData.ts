07450211c19ed18610247c6f5bd32e2436727d43
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-24 18:48:52 +0100

    feat: add registerLocationInputs data items

--- src/data/formData.ts
+++ src/data/formData.ts
@@ -1,5 +1,26 @@
 import { Gender, GenderType } from '@/types';
 
+export const registerLocationInputs = [
+  {
+    id: 1,
+    name: 'street',
+    label: 'Street',
+    placeholder: 'Enter your street',
+  },
+  {
+    id: 2,
+    name: 'city',
+    label: 'City',
+    placeholder: 'Enter your city',
+  },
+  {
+    id: 3,
+    name: 'state',
+    label: 'State',
+    placeholder: 'Enter your state',
+  },
+];
+
 export const options: GenderType = [
   {
     value: Gender.MALE,
