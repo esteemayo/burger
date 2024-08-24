2ec17db88d08bee7bd7030efcd1e72117cdee40e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-08-24 18:40:05 +0100

    fix: implement user authorization to like a product

--- src/app/api/products/like/[id]/route.ts
+++ src/app/api/products/like/[id]/route.ts
@@ -14,48 +14,56 @@ export const PATCH = async (req: NextRequest, { params }: IParams) => {
   const session = await getAuthSession();
 
   if (session) {
-    try {
-      const product = await prisma.product.findUnique({
-        where: {
-          id: productId,
-        },
-      });
+    if (!session.user.isAdmin) {
+      try {
+        const product = await prisma.product.findUnique({
+          where: {
+            id: productId,
+          },
+        });
 
-      if (!product) {
+        if (!product) {
+          return new NextResponse(
+            JSON.stringify({
+              message: `No product found with the given ID → ${productId}`,
+            }),
+            { status: 404 }
+          );
+        }
+
+        const likeIndex = product.likes.findIndex(
+          (userId) => userId === session.user.id
+        );
+
+        if (likeIndex === -1) {
+          product.likes.push(session.user.id);
+        } else {
+          product.likes = product.likes.filter(
+            (userId) => userId !== session.user.id
+          );
+        }
+
+        const updatedProduct = await prisma.product.update({
+          where: {
+            id: productId,
+          },
+          data: { ...product },
+        });
+
+        return new NextResponse(JSON.stringify(updatedProduct), {
+          status: 200,
+        });
+      } catch (err: unknown) {
         return new NextResponse(
-          JSON.stringify({
-            message: `No product found with the given ID → ${productId}`,
-          }),
-          { status: 404 }
+          JSON.stringify({ message: 'Something went wrong' }),
+          { status: 500 }
         );
       }
-
-      const likeIndex = product.likes.findIndex(
-        (userId) => userId === session.user.id
-      );
-
-      if (likeIndex === -1) {
-        product.likes.push(session.user.id);
-      } else {
-        product.likes = product.likes.filter(
-          (userId) => userId !== session.user.id
-        );
-      }
-
-      const updatedProduct = await prisma.product.update({
-        where: {
-          id: productId,
-        },
-        data: { ...product },
-      });
-
-      return new NextResponse(JSON.stringify(updatedProduct), { status: 200 });
-    } catch (err: unknown) {
-      return new NextResponse(
-        JSON.stringify({ message: 'Something went wrong' }),
-        { status: 500 }
-      );
     }
+    return new NextResponse(
+      JSON.stringify({ message: 'You are not authourized' }),
+      { status: 403 }
+    );
   }
   return new NextResponse(
     JSON.stringify({ message: 'You are not authenticated' }),
