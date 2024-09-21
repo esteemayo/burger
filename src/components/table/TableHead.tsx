fae6727b6acbe324ea93397ad9403a808e42ff67
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-09-21 16:43:23 +0100

    feat: add styling

--- src/components/table/TableHead.tsx
+++ src/components/table/TableHead.tsx
@@ -1,14 +1,23 @@
 import { TableHeadProps } from '@/types';
 
 import './Table.scss';
+import { useMemo } from 'react';
 
-const TableHead = ({ columns }: TableHeadProps) => {
+const TableHead = ({ columns, isAdmin }: TableHeadProps) => {
+  const headerClasses = useMemo(() => {
+    return !!isAdmin ? 'tableHead hide' : 'tableHead';
+  }, [isAdmin]);
+
   return (
     <thead>
       <tr>
         {columns.map((column) => {
           const { path, label } = column;
-          return <th key={path}>{label}</th>;
+          return (
+            <th key={path} className={headerClasses}>
+              {label}
+            </th>
+          );
         })}
       </tr>
     </thead>
