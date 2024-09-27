8c46f61baffcf1d73f9af6c7536d5d686a1d5d5b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-09-27 22:49:21 +0100

    fix: modify

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -14,10 +14,19 @@ const Profile = () => {
     <div className='profile'>
       <div className='container'>
         <h1 className='profileHeader'>Your profile</h1>
-        <button className='toggleBtn active' type='button'>
-          <span>&nbsp;</span>
-          <span>&nbsp;</span>
-          <span>&nbsp;</span>
+        <button className='toggleBtn' type='button'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+            className='size-5'
+          >
+            <path
+              fillRule='evenodd'
+              d='M2 6.75A.75.75 0 0 1 2.75 6h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 6.75Zm0 6.5a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z'
+              clipRule='evenodd'
+            />
+          </svg>
         </button>
         <div className='profileWrap'>
           <AccountMenu />
