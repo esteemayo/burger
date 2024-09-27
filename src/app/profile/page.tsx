551f8cd3e55c85831d5f01ee13296a698fa3379e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-09-27 23:25:57 +0100

    fix: refactor

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 
 import AccountMenu from '@/components/accountMenu/AccountMenu';
+import MenuButton from '@/components/menuButton/MenuButton';
 import AccountSettings from '@/components/accountSettings/AccountSettings';
 
 import './Profile.scss';
@@ -14,20 +15,7 @@ const Profile = () => {
     <div className='profile'>
       <div className='container'>
         <h1 className='profileHeader'>Your profile</h1>
-        <button className='toggleBtn' type='button'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 20 20'
-            fill='currentColor'
-            className='size-5'
-          >
-            <path
-              fillRule='evenodd'
-              d='M2 6.75A.75.75 0 0 1 2.75 6h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 6.75Zm0 6.5a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z'
-              clipRule='evenodd'
-            />
-          </svg>
-        </button>
+        <MenuButton />
         <div className='profileWrap'>
           <AccountMenu />
           <AccountSettings />
