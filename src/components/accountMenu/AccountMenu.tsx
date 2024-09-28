1b7cd794125ccacfb0a74abddb95724bca21fb41
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-09-28 19:10:16 +0100

    fix: modify

--- src/components/accountMenu/AccountMenu.tsx
+++ src/components/accountMenu/AccountMenu.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import Link from 'next/link';
 import { Fragment, useCallback, useMemo } from 'react';
-import Image from 'next/image';
+import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 import { profileMenu } from '@/data';
 import { useAccountMenu } from '@/hooks/useAccountMenu';
