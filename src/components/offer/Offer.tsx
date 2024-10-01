fb29af8484f2bf72d745bffc709af9d89cfa524f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-01 01:50:36 +0100

    fix: refactor

--- src/components/offer/Offer.tsx
+++ src/components/offer/Offer.tsx
@@ -1,4 +1,4 @@
-import CountDown from '../countdown/CountDown';
+import OfferInfo from '../offerInfo/OfferInfo';
 import OfferImage from '../offerImage/OfferImage';
 
 import './Offer.scss';
@@ -8,16 +8,7 @@ const Offer = () => {
     <section className='offer'>
       <div className='container'>
         <OfferImage />
-        <div className='right'>
-          <h1 className='heading'>Double crunchy chicken burger</h1>
-          <p className='desc'>
-            Our Double crunchy chicken burger are ultra crispy and absolutely
-            bursting with flavour topped with fresh lettuce, cucumbers, Onions,
-            our special sauce and cheese! Its an absolute crowd pleaser.
-          </p>
-          <CountDown />
-          <button type='button'>Order now</button>
-        </div>
+        <OfferInfo />
       </div>
     </section>
   );
