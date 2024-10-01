3b0b4a782f2bf46859a358668a4612cef6979ea4
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-01 17:34:29 +0100

    fix: modify

--- src/components/offer/Offer.tsx
+++ src/components/offer/Offer.tsx
@@ -1,7 +1,10 @@
+import dynamic from 'next/dynamic';
+
 import OfferInfo from '../offerInfo/OfferInfo';
-import OfferImage from '../offerImage/OfferImage';
 
 import './Offer.scss';
+
+const OfferImage = dynamic(() => import('../offerImage/OfferImage'));
 
 const Offer = () => {
   return (
