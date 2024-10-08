5dfaf8a31f47d1469e17c61b29ac85b7b170fa2b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-08 19:35:58 +0100

    fix: modify

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import RoomServiceIcon from '@mui/icons-material/RoomService';
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 
 import { useSearch } from '@/hooks/useSearch';
 import { useLogout } from '@/hooks/useLogout';
@@ -24,23 +24,12 @@ const Sidebar = () => {
   const { isOpen, handleClose, handleLogout } = useLogout();
   const { searchQuery, handleChange, handleSubmit } = useSearch();
 
-  const closeHandler = useCallback(
-    (e: React.MouseEvent<HTMLElement>) => {
-      const target = e.target as HTMLElement;
-
-      if (!target.classList.contains('sidebar')) {
-        handleClose();
-      }
-    },
-    [handleClose]
-  );
-
   const sidebarClasses = useMemo(() => {
     return !!isOpen ? 'sidebar active' : 'sidebar';
   }, [isOpen]);
 
   return (
-    <aside className={sidebarClasses} onClick={closeHandler}>
+    <aside className={sidebarClasses}>
       <div className='container'>
         <ul className='lists'>
           <li onClick={handleClose}>
