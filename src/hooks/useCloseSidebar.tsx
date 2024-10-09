1d107abb3930b4b5eedd6326831469af16a95659
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-10-09 21:03:03 +0100

    fix: modify

--- src/hooks/useCloseSidebar.tsx
+++ src/hooks/useCloseSidebar.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import { useCallback } from "react";
+import { useCallback } from 'react';
 
-import { useSidebar } from "./useSidebar";
+import { useSidebar } from './useSidebar';
 
 export const useCloseSidebar = () => {
   const onClose = useSidebar((store) => store.onClose);
 
   const closeHandler = useCallback(
-    (e: React.MouseEvent<HTMLElement>) => {
-      const target = e.target as HTMLElement;
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      const target = e.target as HTMLDivElement;
 
       if (!target.classList.contains('sidebar')) {
         onClose();
