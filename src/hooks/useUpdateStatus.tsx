309da1b38ee617d805ef38f8bfefad44600d6da0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-05 16:55:01 +0100

    feat: create updateOrderStatus method

--- src/hooks/useUpdateStatus.tsx
+++ src/hooks/useUpdateStatus.tsx
@@ -4,6 +4,17 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { updateOrder } from '@/services/orderService';
 
+const updateOrderStatus = async ({
+  actionId,
+  status,
+}: {
+  actionId: string;
+  status: string;
+}) => {
+  const { data } = await updateOrder(actionId, { status });
+  return data;
+};
+
 export const useUpdateStatus = () => {
   const queryClient = useQueryClient();
 
